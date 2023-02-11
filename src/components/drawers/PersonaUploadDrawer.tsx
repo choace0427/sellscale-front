8444e8fe9efaf5c70a26c7f8ba3a1db1c9f7553d
Aaron Cassar <aaron@sellscale.com>
2023-02-11 12:36:55 -0800

    Added file upload support

--- src/components/drawers/PersonaUploadDrawer.tsx
+++ src/components/drawers/PersonaUploadDrawer.tsx
@@ -1,67 +1,82 @@
-import { Drawer, Title, Text, Center, Button } from "@mantine/core";
-import { useRef } from "react";
+import { Drawer, Title, Text, Button, Group, FileButton } from "@mantine/core";
+import { useEffect, useRef, useState } from "react";
 import { useRecoilState } from "recoil";
 import { temp_delay } from "../../utils/general";
 import displayNotification from "../../utils/notificationFlow";
 import { uploadDrawerOpenState } from "../atoms/personaAtoms";
 
-
 export default function PersonaUploadDrawer(props: {}) {
-
   const [opened, setOpened] = useRecoilState(uploadDrawerOpenState);
   const isUploading = useRef(false);
+  const resetRef = useRef<() => void>(null);
 
-  const upload = async () => {
-    if(isUploading.current) { return; }
+  const uploadFile = async (file: File) => {
+    if (isUploading.current) {
+      return;
+    }
     isUploading.current = true;
 
     await displayNotification(
-      'make-active-persona',
+      "make-active-persona",
       async () => {
+        console.log(file);
         await temp_delay(1000);
         return Math.random() < 0.75;
       },
       {
         title: `Uploading Persona`,
         message: `Working with servers...`,
-        color: 'teal',
+        color: "teal",
       },
       {
         title: `Uploaded!`,
         message: `Very cool 👍`,
-        color: 'teal',
+        color: "teal",
       },
       {
         title: `Error while uploading!`,
         message: `Test 25% chance of failure!`,
-        color: 'red',
-      },
+        color: "red",
+      }
     );
 
+    resetRef.current?.();
     isUploading.current = false;
-  }
+  };
 
   return (
     <Drawer
-    opened={opened}
-    onClose={() => setOpened(false)}
-    title={<Title order={2}>Upload Persona</Title>}
-    padding="xl"
-    size="xl"
-    position="right"
-  >
-    <Text pb='xs'><span className="font-bold">LinkedIn: </span><span>220/400 prospects available</span></Text>
-    <Text pb='xs'><span className="font-bold">Email: </span><span>220/400 prospects available</span></Text>
+      opened={opened}
+      onClose={() => setOpened(false)}
+      title={<Title order={2}>Upload Persona</Title>}
+      padding="xl"
+      size="xl"
+      position="right"
+    >
+      <Text pb="xs">
+        <span className="font-bold">LinkedIn: </span>
+        <span>220/400 prospects available</span>
+      </Text>
+      <Text pb="xs">
+        <span className="font-bold">Email: </span>
+        <span>220/400 prospects available</span>
+      </Text>
 
-    <Center my='xs'>
-      <Button variant="outline" color="teal" onClick={() => {upload()}}>
-        Upload Now
-      </Button>
-    </Center>
+      <Group position="center">
+        <FileButton resetRef={resetRef} onChange={uploadFile} accept=".csv">
+          {(props) =>
+            <Button
+              {...props}
+              variant="outline"
+              color="teal"
+            >Upload image</Button>
+          }
+        </FileButton>
+      </Group>
 
-    <Text align="center" pb='xs' c="dimmed" fs="italic" fz="sm">CSV of Email + LinkedIn</Text>
-    
-  </Drawer>
+      <Text align="center" pb="xs" c="dimmed" fs="italic" fz="sm">
+        CSV of Email + LinkedIn
+      </Text>
+    </Drawer>
   );
-
 }
