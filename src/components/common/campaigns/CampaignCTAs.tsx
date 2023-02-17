4709a0d694d7944b5792a6beb7113e2d0662a19e
Aaron Cassar <aaronncassar@gmail.com>
2023-02-17 15:03:32 -0800

    Added percentage to campaign CTAs

--- src/components/common/campaigns/CampaignCTAs.tsx
+++ src/components/common/campaigns/CampaignCTAs.tsx
@@ -9,7 +9,7 @@ import { DataTable, DataTableSortStatus } from "mantine-datatable";
 import { useEffect, useRef, useState } from "react";
 
 import { CTA } from "src/main";
-import { chunk } from "lodash";
+import { chunk, sortBy } from "lodash";
 
 const PAGE_SIZE = 10;
 
@@ -21,8 +21,8 @@ export default function CampaignCTAs({
 
   const [page, setPage] = useState(1);
   const [sortStatus, setSortStatus] = useState<DataTableSortStatus>({
-    columnAccessor: "active",
-    direction: "asc",
+    columnAccessor: "percentage",
+    direction: "desc",
   });
   const totalRecords = useRef(0);
 
@@ -34,7 +34,24 @@ export default function CampaignCTAs({
   // Split prospects into pages => data
   const getData = (page: number) => {
     totalRecords.current = ctas.length;
-    return chunk(ctas, PAGE_SIZE)[page - 1];
+    let pageData = chunk(ctas as CTA[], PAGE_SIZE)[page - 1]?.map((cta) => {
+      let totalResponded = 0;
+      if(cta.performance){
+        for(const status in cta.performance.status_map) {
+          if(status !== 'SENT_OUTREACH' && status !== 'NOT_INTERESTED') {
+            totalResponded += cta.performance.status_map[status];
+          }
+        }
+      }
+      return {
+        ...cta,
+        percentage: cta.performance?.total_count ? Math.round((totalResponded / cta.performance.total_count) * 100) : 0,
+      };
+    });
+    if(!pageData) { return []; }
+
+    pageData = sortBy(pageData, sortStatus.columnAccessor);
+    return (sortStatus.direction === 'desc' ? pageData.reverse() : pageData);
   };
 
   return (
@@ -53,6 +70,13 @@ export default function CampaignCTAs({
           {
             accessor: "text_value",
             sortable: true,
+          },
+          {
+            accessor: "percentage",
+            title: "%",
+            sortable: true,
+            width: 80,
+            render: ({ percentage }) => `${percentage}%`,
           },
           {
             accessor: "active",
