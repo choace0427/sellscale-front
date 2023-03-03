4691203a14a4e38b4d3bb8edc8bd9a43deb72979
Aakash Adesara <aakash.adesara@gmail.com>
2023-03-02 20:45:58 -0800

    Follow Up Message

--- src/components/common/prospectDetails/ProspectDetailsViewConversation.tsx
+++ src/components/common/prospectDetails/ProspectDetailsViewConversation.tsx
@@ -1,8 +1,17 @@
 import { userTokenState } from "@atoms/userAtoms";
 import { LinkedInConversationEntry } from "@common/persona/LinkedInConversationEntry";
-import { Button, Flex, Card, Text, Group, ScrollArea } from "@mantine/core";
-import { IconExternalLink } from "@tabler/icons";
+import {
+  Button,
+  Flex,
+  Card,
+  Text,
+  Group,
+  ScrollArea,
+  Textarea,
+} from "@mantine/core";
+import { IconExternalLink, IconMail, IconRobot } from "@tabler/icons";
 import { slice } from "lodash";
+import displayNotification from "@utils/notificationFlow";
 
 import { useState } from "react";
 import { useRecoilValue } from "recoil";
@@ -11,6 +20,7 @@ import { LinkedInMessage } from "src/main";
 type ProspectDetailsViewConversationPropsType = {
   conversation_entry_list: LinkedInMessage[];
   conversation_url: string;
+  prospect_id: number;
 };
 
 const LOAD_CHUNK_SIZE = 5;
@@ -20,6 +30,73 @@ export default function ProspectDetailsViewConversation(
 ) {
   const [msgCount, setMsgCount] = useState(1);
   const userToken = useRecoilValue(userTokenState);
+  const [messageDraft, setMessageDraft] = useState("");
+
+  const sendFollowUp = async () => {
+    await displayNotification(
+      "asend-woz-message",
+      async () => {
+        let result: any = await fetch(
+          `${process.env.REACT_APP_API_URI}/li_conversation/prospect/send_woz_message`,
+          {
+            method: "POST",
+            headers: {
+              Authorization: `Bearer ${userToken}`,
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              new_message: messageDraft,
+              prospect_id: props.prospect_id,
+            }),
+          }
+        ).then(async (e) => {
+          setMessageDraft("");
+          return {
+            status: "success",
+            title: `Success`,
+            message: `Status updated.`,
+          };
+        });
+        return result;
+      },
+      {
+        title: `Scheduling message...`,
+        message: `Queuing your message to be sent.`,
+        color: "teal",
+      },
+      {
+        title: `Scheduled!`,
+        message: `Your message will be sent shortly.`,
+        color: "teal",
+      },
+      {
+        title: `Error while sending message!`,
+        message: `Please contact SellScale engineering team.`,
+        color: "red",
+      }
+    );
+  };
+
+  const generateAIFollowup = () => {
+    setMessageDraft("loading ...");
+    fetch(
+      `${process.env.REACT_APP_API_URI}/li_conversation/prospect/generate_response`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${userToken}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          prospect_id: props.prospect_id,
+        }),
+      }
+    )
+      .then((e) => {
+        return e.json();
+      })
+      .then((resp) => setMessageDraft(resp["message"]));
+  };
 
   // Sorted by date
   const messages = props.conversation_entry_list.sort((a, b) => {
@@ -84,7 +161,6 @@ export default function ProspectDetailsViewConversation(
         </Button>
         {msgCount > 1 && (
           <Button
-            disabled={msgCount >= props.conversation_entry_list.length}
             variant="outline"
             color="gray"
             mt="sm"
@@ -106,6 +182,49 @@ export default function ProspectDetailsViewConversation(
           rightIcon={<IconExternalLink size={14} />}
         >
           Open Conversation
+        </Button>
+      </Flex>
+
+      <Textarea
+        mt="sm"
+        minRows={5}
+        placeholder="Write a follow up message here..."
+        label="Follow Up Message"
+        withAsterisk
+        onChange={(e) => {
+          setMessageDraft(e.target?.value);
+        }}
+        value={messageDraft}
+      />
+      <Flex>
+        {/* <Button
+          variant="outline"
+          mt="sm"
+          color="violet"
+          component="a"
+          mr="sm"
+          target="_blank"
+          fullWidth
+          rel="noopener noreferrer"
+          rightIcon={<IconRobot size={14} />}
+          onClick={() => {
+            generateAIFollowup();
+          }}
+        >
+          Generate AI Follow Up
+        </Button> */}
+        <Button
+          variant="outline"
+          mt="sm"
+          color="blue"
+          component="a"
+          target="_blank"
+          fullWidth
+          rel="noopener noreferrer"
+          rightIcon={<IconMail size={14} />}
+          onClick={sendFollowUp}
+        >
+          Schedule
         </Button>
       </Flex>
     </Card>
