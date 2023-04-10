6483f7485c95a53096fbbaf4cd601c20761551bb
Aakash Adesara <aakash.adesara@gmail.com>
2023-04-10 13:08:08 -0700

    Edit & View CTAs

--- src/components/modals/CTAGeneratorModal.tsx
+++ src/components/modals/CTAGeneratorModal.tsx
@@ -1,4 +1,5 @@
 import { userTokenState } from "@atoms/userAtoms";
+import CTAGeneratorExample from "@common/cta_generator/CTAGeneratorExample";
 import FlexSeparate from "@common/library/FlexSeparate";
 import {
   Text,
@@ -19,6 +20,7 @@ import {
   Badge,
   Flex,
   ScrollArea,
+  Container,
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { ContextModalProps } from "@mantine/modals";
@@ -36,8 +38,8 @@ import { useState } from "react";
 import { useRecoilValue } from "recoil";
 
 interface CTAGeneratorProps extends Record<string, unknown> {
-  personaId: string,
-  personaName: string
+  personaId: string;
+  personaName: string;
 }
 
 export default function CTAGeneratorModal({
@@ -48,7 +50,7 @@ export default function CTAGeneratorModal({
   const theme = useMantineTheme();
   const userToken = useRecoilValue(userTokenState);
   const [generatedCTAs, setGeneratedCTAs] = useState<
-    { cta: string; tag: string, enabled: boolean }[]
+    { cta: string; tag: string; enabled: boolean }[]
   >([]);
   const [loading, setLoading] = useState(false);
 
@@ -70,7 +72,9 @@ export default function CTAGeneratorModal({
     );
     setLoading(false);
     if (result.status === "success") {
-      setGeneratedCTAs(result.extra.map((cta: any) => ({ ...cta, enabled: true })));
+      setGeneratedCTAs(
+        result.extra.map((cta: any) => ({ ...cta, enabled: true }))
+      );
     } else {
       showNotification({
         id: "generate-cta-ideas-error",
@@ -90,6 +94,10 @@ export default function CTAGeneratorModal({
         backgroundColor: theme.colors.dark[7],
       }}
     >
+      <Text size="sm" mb="md">
+        Fill in the form below to generate 6 personalized CTAs.
+      </Text>
+
       <LoadingOverlay visible={loading} overlayBlur={2} />
       <form onSubmit={surveyForm.onSubmit(handleSubmit)}>
         <Stack spacing="xl">
@@ -128,68 +136,109 @@ export default function CTAGeneratorModal({
           {generatedCTAs.length > 0 && (
             <>
               <Divider />
+              <Text>Edit and select the CTAs below.</Text>
+            </>
+          )}
+          {generatedCTAs.length > 0 && (
+            <>
               <ScrollArea h={400}>
                 <Stack>
-                {generatedCTAs.map((cta, index) => (
-                  <FlexSeparate key={index}>
-                    <Badge w={150} mt={10} color={valueToColor(theme, cta.tag)}>
-                      {cta.tag.replace('[', '')}-based
-                    </Badge>
-                    <Flex direction='column'>
-                      <Textarea
-                        w={300}
-                        placeholder="Your CTA"
-                        value={cta.cta}
-                        onChange={(e) => {
-                          const newGeneratedCTAs = [...generatedCTAs];
-                          newGeneratedCTAs[index] = { enabled: cta.enabled, tag: cta.tag, cta: e.currentTarget.value };
-                          setGeneratedCTAs(newGeneratedCTAs);
-                        }}
-                        required
-                        variant="unstyled"
-                        autosize
-                      />
-                      <Text size="xs" color={cta.cta.length <= 120 ? "grey" : "red"}>
-                        {cta.cta.length}/{120}
-                      </Text>
-                    </Flex>
-                    <Button disabled={!cta.enabled} mt={10} onClick={async () => {
-                      if (cta.cta.length > 120) {
-                        showNotification({
-                          id: "create-cta-error",
-                          title: "Error",
-                          message: "CTA must be less than 120 characters",
-                          color: "red",
-                          autoClose: false,
-                        });
-                        return;
-                      }
-                      const response = await createCTA(userToken, innerProps.personaId, cta.cta);
-                      if (response.status === "success") {
-                        showNotification({
-                          id: "create-cta-success",
-                          title: "Success",
-                          message: `Added CTA to Persona`,
-                          color: "green",
-                          autoClose: 5000,
-                        });
+                  {generatedCTAs.map((cta, index) => (
+                    <FlexSeparate key={index}>
+                      <Container>
+                        <Badge
+                          w={150}
+                          mt={10}
+                          color={valueToColor(theme, cta.tag)}
+                        >
+                          {cta.tag.replace("[", "")}-based
+                        </Badge>
+                        <CTAGeneratorExample
+                          ctaText={cta.cta}
+                        ></CTAGeneratorExample>
+                      </Container>
+                      <Flex direction="column">
+                        <Textarea
+                          w={300}
+                          placeholder="Your CTA"
+                          value={cta.cta}
+                          sx={{
+                            border: "solid 1px #333",
+                            padding: 8,
+                            marginRight: 8,
+                          }}
+                          onChange={(e) => {
+                            const newGeneratedCTAs = [...generatedCTAs];
+                            newGeneratedCTAs[index] = {
+                              enabled: cta.enabled,
+                              tag: cta.tag,
+                              cta: e.currentTarget.value,
+                            };
+                            setGeneratedCTAs(newGeneratedCTAs);
+                          }}
+                          required
+                          variant="unstyled"
+                          autosize
+                        />
+                        <Text
+                          size="xs"
+                          color={cta.cta.length <= 120 ? "grey" : "red"}
+                        >
+                          {cta.cta.length}/{120}
+                        </Text>
+                      </Flex>
+                      <Button
+                        disabled={!cta.enabled}
+                        mt={10}
+                        onClick={async () => {
+                          if (cta.cta.length > 120) {
+                            showNotification({
+                              id: "create-cta-error",
+                              title: "Error",
+                              message: "CTA must be less than 120 characters",
+                              color: "red",
+                              autoClose: false,
+                            });
+                            return;
+                          }
+                          const response = await createCTA(
+                            userToken,
+                            innerProps.personaId,
+                            cta.cta
+                          );
+                          if (response.status === "success") {
+                            showNotification({
+                              id: "create-cta-success",
+                              title: "Success",
+                              message: `Added CTA to Persona`,
+                              color: "green",
+                              autoClose: 5000,
+                            });
 
-                        // Disable the CTA
-                        const newGeneratedCTAs = [...generatedCTAs];
-                        newGeneratedCTAs[index] = { enabled: false, tag: cta.tag, cta: cta.cta };
-                        setGeneratedCTAs(newGeneratedCTAs);
-                      } else {
-                        showNotification({
-                          id: "create-cta-error",
-                          title: "Error",
-                          message: "Failed to add CTA to Persona",
-                          color: "red",
-                          autoClose: false,
-                        });
-                      }
-                    }} variant="light">Use CTA</Button>
-                  </FlexSeparate>
-                ))}
+                            // Disable the CTA
+                            const newGeneratedCTAs = [...generatedCTAs];
+                            newGeneratedCTAs[index] = {
+                              enabled: false,
+                              tag: cta.tag,
+                              cta: cta.cta,
+                            };
+                            setGeneratedCTAs(newGeneratedCTAs);
+                          } else {
+                            showNotification({
+                              id: "create-cta-error",
+                              title: "Error",
+                              message: "Failed to add CTA to Persona",
+                              color: "red",
+                              autoClose: false,
+                            });
+                          }
+                        }}
+                        variant="light"
+                      >
+                        Use CTA
+                      </Button>
+                    </FlexSeparate>
+                  ))}
                 </Stack>
               </ScrollArea>
             </>
