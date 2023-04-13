357ab26af6730593c3f6d582dc3d3bc9c235339f
Aaron Cassar <aaron@sellscale.com>
2023-04-13 15:32:34 -0500

    Updated version number

--- src/main.tsx
+++ src/main.tsx
@@ -29,7 +29,7 @@ import { BrowserTracing } from "@sentry/tracing";
 import LinkedInPage from "@pages/LinkedInPage";
 import EmailPage from "@pages/EmailPage";
 
-console.log('Here is the version: 1.1.0');
+console.log('Here is the version: 1.1.1');
 
 const queryClient = new QueryClient();
 
