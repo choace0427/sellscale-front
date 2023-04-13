24780c7e9bda87507695b58f717bc594d0a4865b
Aaron Cassar <aaron@sellscale.com>
2023-04-12 22:48:39 -0500

    Added sending errors to slack

--- src/utils/requests/utils.ts
+++ src/utils/requests/utils.ts
@@ -1,4 +1,5 @@
 import { logout } from "@auth/core";
+import { API_URL } from "@constants/data";
 import { showNotification } from "@mantine/notifications";
 import { MsgResponse } from "src";
 
@@ -8,20 +9,15 @@ export default async function getResponseJSON(key: string, response: Response): 
     logout();
     return { status: 'error', title: `Not Authorized`, message: 'Please login again.' };
   }
+
+  let errMsg = '';
   const res = await response.json().catch((error) => {
-    console.error(error);
-    showNotification({
-      id: key+"-error",
-      title: "Error",
-      message: `Error: ${error.length > 20 ? 'Issue occurred, please check logs' : error}`,
-      color: "red",
-      autoClose: false,
-    });
+    errMsg = error;
   });
   if (!res) {
-    return { status: 'error', title: `Error`, message: `See logs for details` };
+    errMsg = 'Unknown error';
   }
-  if (response.status === 403 && res.message.toLowerCase() === "invalid linkedin cookies") {
+  if (response.status === 403 && res && res.message.toLowerCase() === "invalid linkedin cookies") {
     showNotification({
       id: key+"-invalid-li-cookies",
       title: "LinkedIn Disconnected",
@@ -31,15 +27,38 @@ export default async function getResponseJSON(key: string, response: Response): 
     });
     return { status: 'error', title: `LinkedIn Disconnected`, message: `Looks like your LinkedIn account has been disconnected. Please reconnect your LinkedIn account.` };
   }
-  if (response.status !== 200) {
+
+  if (!(response.status+'').startsWith('2')) {
+    errMsg = `Responded with: ${response.status}, ${response.statusText} \n Res: ${JSON.stringify(res)}`;
+  }
+
+  if (errMsg) {
     showNotification({
-      id: key+"-not-okay",
+      id: key+"-error",
       title: "Error",
-      message: `Responded with: ${response.status}, ${response.statusText}`,
+      message: `An error occurred, our engineers have been notified. Please try again later.`,
       color: "red",
       autoClose: false,
     });
-    return { status: 'error', title: `Error`, message: `Responded with: ${response.status}, ${response.statusText}` };
+
+    const userToken = localStorage.getItem("user-token");
+    if(userToken){
+      await fetch(
+        `${API_URL}/client/submit-error`,
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${userToken}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            "error": errMsg,
+            "user_agent": navigator.userAgent.toLowerCase()
+          }),
+        }
+      );
+    }
+
   }
 
   return res;
