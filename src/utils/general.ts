07cb3811878042fadc1178f3b9a744c342ce20ae
Aaron Cassar <aaron@sellscale.com>
2023-04-24 19:02:15 -0700

    Minor bug fix

--- src/utils/general.ts
+++ src/utils/general.ts
@@ -119,6 +119,7 @@ const PRESET_COLOR_MAPS: Record<string, MantineColor> = {
   active_convo_qual_needed: "yellow",
   active_convo_question: "yellow",
   active_convo_scheduling: "yellow",
+  active_convo_next_steps: "yellow",
   bumped: "orange",
   demo: "blue",
   demo_complete: "green",
