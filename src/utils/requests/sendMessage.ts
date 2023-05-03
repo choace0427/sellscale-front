74cc92962e02a2382e02c9bad193ebdb64d82f5e
Aakash Adesara <aakash.adesara@gmail.com>
2023-05-03 13:56:50 -0700

    hotfix

--- src/utils/requests/sendMessage.ts
+++ src/utils/requests/sendMessage.ts
@@ -4,32 +4,34 @@ import { API_URL } from "@constants/data";
 
 /**
  * Send LinkedIn message to prospect
- * @param userToken 
- * @param prospectId 
- * @param message 
+ * @param userToken
+ * @param prospectId
+ * @param message
  * @param purgatory
  * @returns - MsgResponse
  */
-export async function sendLinkedInMessage(userToken: string, prospectId: number, message: string, purgatory?: boolean): Promise<MsgResponse> {
-
-  const response = await fetch(
-    `${API_URL}/voyager/send_message`,
-    {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${userToken}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        "prospect_id": prospectId,
-        "message": message,
-        "purgatory": purgatory ?? false,
-      }),
-    }
-  );
+export async function sendLinkedInMessage(
+  userToken: string,
+  prospectId: number,
+  message: string,
+  purgatory?: boolean
+): Promise<MsgResponse> {
+  const response = await fetch(`${API_URL}/voyager/send_message`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${userToken}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prospect_id: prospectId,
+      message: message,
+      purgatory: purgatory ?? true,
+    }),
+  });
   const result = await getResponseJSON("send-linkedin-message", response);
-  if(isMsgResponse(result)) { return result; }
+  if (isMsgResponse(result)) {
+    return result;
+  }
 
-  return { status: 'success', title: `Success`, message: `Sent message` };
-
+  return { status: "success", title: `Success`, message: `Sent message` };
 }
