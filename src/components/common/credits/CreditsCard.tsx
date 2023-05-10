84a43a29fa2dc68bea891cf5321d3e82874bbe21
David Wei <49255196+davidmwei@users.noreply.github.com>
2023-05-10 14:12:20 -0700

    Update CreditsCard.tsx (#120)

--- src/components/common/credits/CreditsCard.tsx
+++ src/components/common/credits/CreditsCard.tsx
@@ -1,4 +1,4 @@
-import { userTokenState } from "@atoms/userAtoms";
+import { userDataState, userTokenState } from "@atoms/userAtoms";
 import { API_URL } from "@constants/data";
 import { Title, Text, Card, Divider } from "@mantine/core";
 import { Progress } from "@mantine/core";
@@ -6,27 +6,30 @@ import { useEffect, useState } from "react";
 import { useRecoilValue } from "recoil";
 
 export default function CreditsCard() {
-  const [numEmailFetchingCredits, setNumEmailFetchingCredits] = useState(0);
-  const userToken = useRecoilValue(userTokenState);
+  // const [numEmailFetchingCredits, setNumEmailFetchingCredits] = useState(0);
+  // const userToken = useRecoilValue(userTokenState);
+
+  const userData = useRecoilValue(userDataState);
 
   const MAX_NUM_EMAIL_FETCHING_CREDITS = 2000;
+  const MAX_NUM_ML_RESEARCH_CREDITS = 5000;
 
-  useEffect(() => {
-    const fetchCredits = async () => {
-      const res = await fetch(
-        `${API_URL}/prospect/get_credits`,
-        {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${userToken}`,
-          },
-        }
-      );
-      const data = await res.json();
-      setNumEmailFetchingCredits(data.email_fetching_credits);
-    };
-    fetchCredits();
-  }, []);
+  // useEffect(() => {
+  //   const fetchCredits = async () => {
+  //     const res = await fetch(
+  //       `${API_URL}/prospect/get_credits`,
+  //       {
+  //         method: "GET",
+  //         headers: {
+  //           Authorization: `Bearer ${userToken}`,
+  //         },
+  //       }
+  //     );
+  //     const data = await res.json();
+  //     setNumEmailFetchingCredits(data.email_fetching_credits);
+  //   };
+  //   fetchCredits();
+  // }, []);
 
   return (
     <Card pl="sm" pr="sm">
@@ -35,13 +38,25 @@ export default function CreditsCard() {
       </Title>
       <Text size="sm">Email Extraction Credits</Text>
       <Progress
-        value={(numEmailFetchingCredits / MAX_NUM_EMAIL_FETCHING_CREDITS) * 100}
+        value={(userData.email_fetching_credits / MAX_NUM_EMAIL_FETCHING_CREDITS) * 100}
         animate
         size={"lg"}
         color="pink"
       />
       <Text size="xs" mb="md">
-        {numEmailFetchingCredits} / {MAX_NUM_EMAIL_FETCHING_CREDITS} credits
+        {userData.email_fetching_credits} / {MAX_NUM_EMAIL_FETCHING_CREDITS} credits
+        left for month
+      </Text>
+
+      <Text size="sm">ML Research Credits</Text>
+      <Progress
+        value={(userData.ml_credits / MAX_NUM_ML_RESEARCH_CREDITS) * 100}
+        animate
+        size={"lg"}
+        color="pink"
+      />
+      <Text size="xs" mb="md">
+        {userData.ml_credits} / {MAX_NUM_ML_RESEARCH_CREDITS} credits
         left for month
       </Text>
     </Card>
