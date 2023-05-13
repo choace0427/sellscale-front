b286a308fb80617f43093f172a67a104f22e6b4b
Aakash Adesara <aakash.adesara@gmail.com>
2023-05-12 20:21:35 -0700

    hotfix

--- src/components/pages/LinkedInPage.tsx
+++ src/components/pages/LinkedInPage.tsx
@@ -10,7 +10,15 @@ import PersonaDetailsCTAs from "@common/persona/details/PersonaDetailsCTAs";
 import PersonaDetailsTransformers from "@common/persona/details/PersonaDetailsTransformers";
 import { Select, Tabs } from "@mantine/core";
 import PageTitle from "@nav/PageTitle";
-import { IconAffiliate, IconHistory, IconMailFast, IconSignature, IconSpeakerphone, IconTopologyStar, IconUser } from "@tabler/icons";
+import {
+  IconAffiliate,
+  IconHistory,
+  IconMailFast,
+  IconSignature,
+  IconSpeakerphone,
+  IconTopologyStar,
+  IconUser,
+} from "@tabler/icons";
 import { setPageTitle } from "@utils/documentChange";
 import getPersonas from "@utils/requests/getPersonas";
 import { useQuery } from "@tanstack/react-query";
@@ -24,7 +32,9 @@ export default function LinkedInPage() {
 
   const { tabId } = useLoaderData() as { tabId: string };
   const userToken = useRecoilValue(userTokenState);
-  const [currentPersonaId, setCurrentPersonaId] = useRecoilState(currentPersonaIdState);
+  const [currentPersonaId, setCurrentPersonaId] = useRecoilState(
+    currentPersonaIdState
+  );
 
   const { data, isFetching, refetch } = useQuery({
     queryKey: [`query-linkedin-personas-data`],
@@ -33,15 +43,21 @@ export default function LinkedInPage() {
       const result =
         response.status === "success" ? (response.data as Archetype[]) : [];
 
+      console.log(result);
+
       const personas = result.sort((a, b) => {
         if (a.active === b.active) {
-          return b.performance.total_prospects - a.performance.total_prospects;
+          return (
+            b.performance?.total_prospects - a.performance?.total_prospects
+          );
         } else {
           return a.active ? -1 : 1;
         }
       });
 
-      if(currentPersonaId === -1){
+      console.log(personas);
+
+      if (currentPersonaId === -1) {
         setCurrentPersonaId(personas[0].id);
       }
       return personas;
@@ -49,14 +65,30 @@ export default function LinkedInPage() {
     refetchOnWindowFocus: false,
   });
 
+  console.log(data);
+
   return (
     <PageFrame>
       <Tabs value={tabId} px="xs" color="teal">
-        <Tabs.List sx={{ display: 'none' }}>
-          <Tabs.Tab value="messages" icon={<IconMailFast size="1.1rem" />}>Messages</Tabs.Tab>
-          <Tabs.Tab value="ctas" icon={<IconSpeakerphone size="1.1rem" />}>CTAs</Tabs.Tab>
-          <Tabs.Tab value="personalizations" icon={<IconAffiliate size="1.1rem" />}>Personalizations</Tabs.Tab>
-          <Tabs.Tab value="campaign-history" icon={<IconHistory size="1.1rem" />}>Campaign History</Tabs.Tab>
+        <Tabs.List sx={{ display: "none" }}>
+          <Tabs.Tab value="messages" icon={<IconMailFast size="1.1rem" />}>
+            Messages
+          </Tabs.Tab>
+          <Tabs.Tab value="ctas" icon={<IconSpeakerphone size="1.1rem" />}>
+            CTAs
+          </Tabs.Tab>
+          <Tabs.Tab
+            value="personalizations"
+            icon={<IconAffiliate size="1.1rem" />}
+          >
+            Personalizations
+          </Tabs.Tab>
+          <Tabs.Tab
+            value="campaign-history"
+            icon={<IconHistory size="1.1rem" />}
+          >
+            Campaign History
+          </Tabs.Tab>
         </Tabs.List>
         <Tabs.Panel value="messages" pt="xs">
           <LinkedinQueuedMessages />
@@ -65,26 +97,26 @@ export default function LinkedInPage() {
           <PersonaDetailsCTAs personas={data} />
         </Tabs.Panel>
         <Tabs.Panel value="personalizations" pt="xs">
-            <FlexSeparate alignItems="flex-end">
-              <div></div>
-              <Select
-                pr="sm"
-                pb="xs"
-                placeholder="Select a persona"
-                color="teal"
-                // @ts-ignore
-                data={
-                  data
-                    ? data.map((persona: Archetype) => ({
-                        value: persona.id+'',
-                        label: persona.archetype,
-                      }))
-                    : []
-                }
-                icon={<IconUser size="1rem" />}
-                value={currentPersonaId+''}
-                onChange={(value) => setCurrentPersonaId(value ? +value : -1)}
-              />
+          <FlexSeparate alignItems="flex-end">
+            <div></div>
+            <Select
+              pr="sm"
+              pb="xs"
+              placeholder="Select a persona"
+              color="teal"
+              // @ts-ignore
+              data={
+                data
+                  ? data.map((persona: Archetype) => ({
+                      value: persona.id + "",
+                      label: persona.archetype,
+                    }))
+                  : []
+              }
+              icon={<IconUser size="1rem" />}
+              value={currentPersonaId + ""}
+              onChange={(value) => setCurrentPersonaId(value ? +value : -1)}
+            />
           </FlexSeparate>
           <PersonaDetailsTransformers channel="LINKEDIN" />
         </Tabs.Panel>
@@ -94,5 +126,4 @@ export default function LinkedInPage() {
       </Tabs>
     </PageFrame>
   );
-
 }
