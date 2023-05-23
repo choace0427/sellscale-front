e262dcca70047bc7b89953d5a3802fd9831eca00
Aaron Cassar <aaron@sellscale.com>
2023-05-22 21:22:59 -0700

    Fixed calendly auth bug

--- src/components/pages/AuthCalendlyPage.tsx
+++ src/components/pages/AuthCalendlyPage.tsx
@@ -1,40 +1,36 @@
-import { userDataState, userTokenState } from "@atoms/userAtoms";
-import { authorize } from "@auth/core";
-import {
-  LoadingOverlay,
-} from "@mantine/core";
-import { showNotification } from "@mantine/notifications";
-import { navigateToPage } from "@utils/documentChange";
-import { useEffect, useRef } from "react";
-import { useNavigate, useSearchParams } from "react-router-dom";
-import { useRecoilState, useRecoilValue } from "recoil";
-import PageFrame from "../common/PageFrame";
-import { API_URL } from "@constants/data";
-import { updateCalendlyAccessToken } from "@utils/requests/updateCalendlyAccessToken";
-
+import { userDataState, userTokenState } from '@atoms/userAtoms';
+import { authorize } from '@auth/core';
+import { LoadingOverlay } from '@mantine/core';
+import { showNotification } from '@mantine/notifications';
+import { navigateToPage } from '@utils/documentChange';
+import { useEffect, useRef } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import PageFrame from '../common/PageFrame';
+import { API_URL } from '@constants/data';
+import { updateCalendlyAccessToken } from '@utils/requests/updateCalendlyAccessToken';
+import { useDebouncedValue, useDidUpdate } from '@mantine/hooks';
 
 export default function AuthCalendlyPage() {
-
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const [userData, setUserData] = useRecoilState(userDataState);
   const [userToken, setUserToken] = useRecoilState(userTokenState);
 
   const code = searchParams.get('code');
-  const codeRef = useRef('');
+  const codeCallRef = useRef(true);
 
-  if(code){
-    codeRef.current = code;
-    (async () => {
-      console.log(codeRef.current);
-      const response = await updateCalendlyAccessToken(userToken, codeRef.current);
-      console.log(codeRef.current);
-      console.log(response);
-      if(response.status === 'success'){
-        navigateToPage(navigate, '/');
-      }
-    })();
-  }
+  useEffect(() => {
+    if (code && codeCallRef.current) {
+      codeCallRef.current = false;
+      (async () => {
+        const response = await updateCalendlyAccessToken(userToken, code);
+        if(response.status === 'success'){
+          navigateToPage(navigate, '/');
+        }
+      })();
+    }
+  }, []);
 
   return (
     <PageFrame>
