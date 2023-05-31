cf141e726d8db70cb8e1e4b5eecac30721d8da29
Aakash Adesara <aakash.adesara@gmail.com>
2023-05-31 13:11:22 -0700

    More Data for Fetch Requests

--- src/components/common/persona/PersonaRecentSplitRequests.tsx
+++ src/components/common/persona/PersonaRecentSplitRequests.tsx
@@ -1,6 +1,17 @@
-import { forwardRef, useEffect } from "react";
-import { MultiSelect, Box, Badge, Text, Title, Card } from "@mantine/core";
-import { useQuery } from "@tanstack/react-query";
+import { forwardRef, useEffect, useState } from "react";
+import {
+  MultiSelect,
+  Box,
+  Badge,
+  Text,
+  Title,
+  Card,
+  Divider,
+  Flex,
+  Button,
+  Loader,
+} from "@mantine/core";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useRecoilState } from "recoil";
 import { userTokenState } from "@atoms/userAtoms";
 import { logout } from "@auth/core";
@@ -12,44 +23,90 @@ type PropsType = {
 
 export default function PersonaRecentSplitRequests(props: PropsType) {
   const [userToken] = useRecoilState(userTokenState);
+  const queryClient = useQueryClient();
 
-  const { data, error, isLoading } = useQuery(
-    ["personaRecentSplitRequests", props.archetype_id],
-    () =>
-      fetch(
-        `${API_URL}/personas/recent_split_requests?source_archetype_id=${props.archetype_id}`,
-        {
-          headers: {
-            Authorization: `Bearer ${userToken}`,
-            "Content-Type": "application/json",
-          },
-        }
-      ).then((res) => {
+  const [loadingFetchRequests, setLoadingFetchRequests] = useState(false);
+  const [fetchedSplitRequests, setFetchedSplitRequests] = useState(false);
+  const [fetchRequests, setFetchRequests]: any = useState({});
+
+  const fetchSplitRequests = () => {
+    setLoadingFetchRequests(true);
+    fetch(
+      `${API_URL}/personas/recent_split_requests?source_archetype_id=${props.archetype_id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${userToken}`,
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((res) => {
         if (res.status === 401) {
           logout();
         }
         return res.json();
       })
-  );
+      .then((j) => {
+        setFetchRequests(j);
+      })
+      .finally(() => {
+        setLoadingFetchRequests(false);
+      });
+  };
+
+  useEffect(() => {
+    if (!fetchedSplitRequests) {
+      fetchSplitRequests();
+      setFetchedSplitRequests(true);
+    }
+  }, []);
 
   return (
     <Box>
-      {data?.recent_requests.length > 0 && (
-        <Title order={4} mb="sm">
-          Recent Split Requests
-        </Title>
+      {fetchRequests?.recent_requests?.length > 0 && (
+        <Flex direction="row">
+          <Title order={4} mb="sm">
+            Recent Split Requests
+          </Title>
+          <Button
+            variant="subtle"
+            onClick={() => {
+              fetchSplitRequests();
+            }}
+          >
+            Refresh
+          </Button>
+        </Flex>
       )}
-      {data?.recent_requests.map((entry: any) => {
-        return (
-          <Card w="100%" withBorder>
-            <Badge>{entry.created_at}</Badge>
-            <Text mt="xs">
-              Transferred prospects into{" "}
-              {entry.destination_client_archetype_ids?.length || 0} personas
-            </Text>
-          </Card>
-        );
-      })}
+      {loadingFetchRequests && <Loader variant="dots" />}
+      {!loadingFetchRequests &&
+        fetchRequests?.recent_requests &&
+        fetchRequests?.recent_requests.map((entry: any) => {
+          var total_prospect_count =
+            entry.num_queued +
+            entry.num_in_progress +
+            entry.num_completed +
+            entry.num_failed;
+          return (
+            <Card w="100%" withBorder mt="xs">
+              <Badge color="grape">{entry.created_at}</Badge>
+              <Text mt="xs">
+                Attempted to split {total_prospect_count} prospects into{" "}
+                {entry.destination_client_archetype_ids?.length || 0} personas
+              </Text>
+              <Divider />
+              <Badge mt="xs" color="yellow" mr="xs">
+                {entry.num_in_progress} queued
+              </Badge>
+              <Badge mt="xs" color="red" mr="xs">
+                {entry.num_failed} failed
+              </Badge>
+              <Badge mt="xs" color="green">
+                {entry.num_completed} completed
+              </Badge>
+            </Card>
+          );
+        })}
     </Box>
   );
 }
