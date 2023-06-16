63142e02268bd9653d9496274edbd4730dc07813
Aakash Adesara <aakash.adesara@gmail.com>
2023-06-16 08:38:13 -0700

    Add Length Counter to Voice Builder

--- src/components/common/voice_builder/VoiceBuilderFlow.tsx
+++ src/components/common/voice_builder/VoiceBuilderFlow.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import {
   Checkbox,
   Group,
@@ -21,24 +21,38 @@ import {
   Divider,
   ScrollArea,
   Tooltip,
-} from '@mantine/core';
-import { Archetype, Prospect } from 'src';
-import { IconEdit, IconHeart, IconHeartOff, IconPencil, IconRefresh, IconTrash } from '@tabler/icons';
-import { getHotkeyHandler, useDebouncedState } from '@mantine/hooks';
-import { useRecoilState, useRecoilValue } from 'recoil';
-import { voiceBuilderMessagesState } from '@atoms/voiceAtoms';
-import { MAX_EDITING_PHASES, MSG_GEN_AMOUNT, STARTING_INSTRUCTIONS } from '@modals/VoiceBuilderModal';
-import { userTokenState } from '@atoms/userAtoms';
+} from "@mantine/core";
+import { Archetype, Prospect } from "src";
+import {
+  IconEdit,
+  IconHeart,
+  IconHeartOff,
+  IconPencil,
+  IconRefresh,
+  IconTrash,
+} from "@tabler/icons";
+import { getHotkeyHandler, useDebouncedState } from "@mantine/hooks";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { voiceBuilderMessagesState } from "@atoms/voiceAtoms";
+import {
+  MAX_EDITING_PHASES,
+  MSG_GEN_AMOUNT,
+  STARTING_INSTRUCTIONS,
+} from "@modals/VoiceBuilderModal";
+import { userTokenState } from "@atoms/userAtoms";
 import {
   createVoice,
   deleteSample,
   generateSamples,
   updateOnboardingInstructions,
   updateSample,
-} from '@utils/requests/voiceBuilder';
-import { prospectDrawerIdState, prospectDrawerOpenState } from '@atoms/prospectAtoms';
-import _ from 'lodash';
-import { el } from '@fullcalendar/core/internal-common';
+} from "@utils/requests/voiceBuilder";
+import {
+  prospectDrawerIdState,
+  prospectDrawerOpenState,
+} from "@atoms/prospectAtoms";
+import _ from "lodash";
+import { el } from "@fullcalendar/core/internal-common";
 
 const ItemComponent = (props: { id: number; defaultValue: string }) => {
   const [message, setMessage] = useState<string>(props.defaultValue);
@@ -47,12 +61,18 @@ const ItemComponent = (props: { id: number; defaultValue: string }) => {
   const [_opened, setProspectOpened] = useRecoilState(prospectDrawerOpenState);
   const [_prospectId, setProspectId] = useRecoilState(prospectDrawerIdState);
 
-  const [voiceBuilderMessages, setVoiceBuilderMessages] = useRecoilState(voiceBuilderMessagesState);
+  const [voiceBuilderMessages, setVoiceBuilderMessages] = useRecoilState(
+    voiceBuilderMessagesState
+  );
 
-  const existingMessage = voiceBuilderMessages.find((item) => item.id === props.id);
+  const existingMessage = voiceBuilderMessages.find(
+    (item) => item.id === props.id
+  );
 
   const saveMessages = (newMessage?: string) => {
-    const oldMessage = voiceBuilderMessages.find((item) => item.id === props.id);
+    const oldMessage = voiceBuilderMessages.find(
+      (item) => item.id === props.id
+    );
     if (oldMessage) {
       // Update global state list and set new local state message
       setVoiceBuilderMessages(
@@ -73,8 +93,8 @@ const ItemComponent = (props: { id: number; defaultValue: string }) => {
     }
   };
 
-  if(!editing && !message){
-    return (<></>);
+  if (!editing && !message) {
+    return <></>;
   }
 
   return (
@@ -82,8 +102,8 @@ const ItemComponent = (props: { id: number; defaultValue: string }) => {
       <Group noWrap spacing={0}>
         <div style={{ flexGrow: 1 }}>
           <Anchor
-            component='button'
-            type='button'
+            component="button"
+            type="button"
             onClick={() => {
               if (existingMessage?.prospect) {
                 setProspectId(existingMessage.prospect.id);
@@ -91,7 +111,7 @@ const ItemComponent = (props: { id: number; defaultValue: string }) => {
               }
             }}
           >
-            {existingMessage?.prospect?.full_name || 'Example Prospect'}
+            {existingMessage?.prospect?.full_name || "Example Prospect"}
           </Anchor>
           <Container
             m={0}
@@ -105,16 +125,16 @@ const ItemComponent = (props: { id: number; defaultValue: string }) => {
           >
             {editing ? (
               <Textarea
-                size='xs'
+                size="xs"
                 autosize
-                variant='unstyled'
+                variant="unstyled"
                 onChange={(e) => {
                   setMessage(e.currentTarget.value);
                 }}
                 value={message}
                 onKeyDown={getHotkeyHandler([
                   [
-                    'Enter', //mod+Enter
+                    "Enter", //mod+Enter
                     () => {
                       setEditing(false);
                       saveMessages();
@@ -123,21 +143,28 @@ const ItemComponent = (props: { id: number; defaultValue: string }) => {
                 ])}
                 styles={{
                   input: {
-                    padding: '0!important',
-                    paddingTop: '0!important',
-                    paddingBottom: '0!important',
+                    padding: "0!important",
+                    paddingTop: "0!important",
+                    paddingBottom: "0!important",
                   },
                 }}
               />
             ) : (
-              <Text size='xs' color='dimmed'>
+              <Text size="xs" color="dimmed">
                 {message}
               </Text>
             )}
           </Container>
+          <Text
+            align="right"
+            size="xs"
+            color={message.length > 300 ? "red" : "dimmed"}
+          >
+            {message.length} / 300
+          </Text>
         </div>
         <div style={{ flexGrow: 0 }}>
-          <Tooltip label='Edit' position='left' openDelay={500} withArrow>
+          <Tooltip label="Edit" position="left" openDelay={500} withArrow>
             <ActionIcon
               onClick={(e) => {
                 e.preventDefault();
@@ -146,30 +173,35 @@ const ItemComponent = (props: { id: number; defaultValue: string }) => {
                 saveMessages();
               }}
             >
-              <IconPencil size='1.125rem' />
+              <IconPencil size="1.125rem" />
             </ActionIcon>
           </Tooltip>
-          <Tooltip label='Discard' position='left' openDelay={500} withArrow>
+          <Tooltip label="Discard" position="left" openDelay={500} withArrow>
             <ActionIcon
               onClick={(e) => {
                 e.preventDefault();
                 e.stopPropagation();
                 setEditing(false);
-                saveMessages('');
+                saveMessages("");
               }}
             >
-              <IconTrash size='1.125rem' />
+              <IconTrash size="1.125rem" />
             </ActionIcon>
           </Tooltip>
         </div>
       </Group>
-      <Divider my='sm' />
+      <Divider my="sm" />
     </Container>
   );
 };
 
-export default function VoiceBuilderFlow(props: { persona: Archetype; voiceBuilderOnboardingId: number }) {
-  const [voiceBuilderMessages, setVoiceBuilderMessages] = useRecoilState(voiceBuilderMessagesState);
+export default function VoiceBuilderFlow(props: {
+  persona: Archetype;
+  voiceBuilderOnboardingId: number;
+}) {
+  const [voiceBuilderMessages, setVoiceBuilderMessages] = useRecoilState(
+    voiceBuilderMessagesState
+  );
   const userToken = useRecoilValue(userTokenState);
 
   // Set global state to loaded messages
@@ -182,7 +214,7 @@ export default function VoiceBuilderFlow(props: { persona: Archetype; voiceBuild
 
   const [editingPhase, setEditingPhase] = useState(1);
   const [loadingMsgGen, setLoadingMsgGen] = useState(false);
-  const [instructions, setInstructions] = useDebouncedState('', 200);
+  const [instructions, setInstructions] = useDebouncedState("", 200);
 
   const canCreate = editingPhase >= MAX_EDITING_PHASES;
 
@@ -204,7 +236,7 @@ export default function VoiceBuilderFlow(props: { persona: Archetype; voiceBuild
 
     // Delete all samples that are empty
     for (const message of currentMessages) {
-      if(message.value === ''){
+      if (message.value === "") {
         await deleteSample(userToken, message.id);
       } else {
         await updateSample(userToken, message.id, message.value);
@@ -212,7 +244,11 @@ export default function VoiceBuilderFlow(props: { persona: Archetype; voiceBuild
     }
 
     // Generate new samples
-    const response = await generateSamples(userToken, props.voiceBuilderOnboardingId, MSG_GEN_AMOUNT);
+    const response = await generateSamples(
+      userToken,
+      props.voiceBuilderOnboardingId,
+      MSG_GEN_AMOUNT
+    );
     console.log(response);
 
     // Delete all old samples
@@ -221,11 +257,15 @@ export default function VoiceBuilderFlow(props: { persona: Archetype; voiceBuild
       deleteSample(userToken, message.id);
     }
 
-    if (response.status === 'success') {
+    if (response.status === "success") {
       // Replace global state with only new samples
       setVoiceBuilderMessages((prev) => {
         return response.data.map((item: any) => {
-          return { id: item.id, value: item.sample_completion, prospect: item.prospect };
+          return {
+            id: item.id,
+            value: item.sample_completion,
+            prospect: item.prospect,
+          };
         });
       });
     }
@@ -239,36 +279,44 @@ export default function VoiceBuilderFlow(props: { persona: Archetype; voiceBuild
     <>
       <Textarea
         placeholder={`- Please adjust titles to be less formal (i.e. lowercase, acronyms).\n- Avoid using the word 'impressive'.`}
-        label='Additional Instructions'
+        label="Additional Instructions"
         minRows={2}
         onChange={(e) => setInstructions(e.currentTarget.value)}
       />
-      <div style={{ position: 'relative' }}>
-        <Text pt={15} px={2} fz='sm' fw={500}>
-          Please edit these messages to your liking or click to discard. Once you’re satisfied, click to continue.
+      <div style={{ position: "relative" }}>
+        <Text pt={15} px={2} fz="sm" fw={500}>
+          Please edit these messages to your liking or click to discard. Once
+          you’re satisfied, click to continue.
         </Text>
-        <ScrollArea style={{ position: 'relative', height: 410 }}>
-          <LoadingOverlay variant='bars' visible={loadingMsgGen} />
+        <ScrollArea style={{ position: "relative", height: 410 }}>
+          <LoadingOverlay variant="bars" visible={loadingMsgGen} />
           <Container>
-            <Divider my='sm' />
+            <Divider my="sm" />
           </Container>
           {voiceBuilderMessages.map((item) => (
-            <ItemComponent key={item.id} id={item.id} defaultValue={item.value} />
+            <ItemComponent
+              key={item.id}
+              id={item.id}
+              defaultValue={item.value}
+            />
           ))}
         </ScrollArea>
       </div>
       <Center mt={10}>
         <Button
-          radius='xl'
-          size='md'
-          m='auto'
+          radius="xl"
+          size="md"
+          m="auto"
           compact
-          variant='light'
+          variant="light"
           disabled={loadingMsgGen}
           onClick={async () => {
             if (canCreate) {
-              const response = await createVoice(userToken, props.voiceBuilderOnboardingId);
-              if (response.status === 'success') {
+              const response = await createVoice(
+                userToken,
+                props.voiceBuilderOnboardingId
+              );
+              if (response.status === "success") {
                 window.location.href = `/linkedin/voices`;
               }
             } else {
@@ -277,7 +325,9 @@ export default function VoiceBuilderFlow(props: { persona: Archetype; voiceBuild
             }
           }}
         >
-          {canCreate ? `Create Voice` : `Continue ${editingPhase}/${MAX_EDITING_PHASES}`}
+          {canCreate
+            ? `Create Voice`
+            : `Continue ${editingPhase}/${MAX_EDITING_PHASES}`}
         </Button>
       </Center>
     </>
