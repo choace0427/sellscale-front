e1025b1488503439a15836fc7fb6e8412c6ec1f3
Aaron Cassar <aaron@sellscale.com>
2023-07-11 23:07:03 -0700

    Added AI disabled to ui

--- src/components/common/inbox/InboxProspectConvo.tsx
+++ src/components/common/inbox/InboxProspectConvo.tsx
@@ -175,6 +175,8 @@ export default function ProspectConvo(props: { prospects: Prospect[] }) {
 
   const linkedin_public_id = data?.li.li_profile?.split('/in/')[1]?.split('/')[0] ?? '';
 
+  const ai_disabled = !prospect || (prospect.li_last_message_from_sdr !== null && userData.disable_ai_on_message_send) || (prospect.li_last_message_from_prospect !== null && userData.disable_ai_on_prospect_respond);
+
   return (
     <Flex gap={0} direction='column' wrap='nowrap' h={'100%'} bg='white'>
       <div style={{ height: HEADER_HEIGHT, position: 'relative' }}>
@@ -229,8 +231,10 @@ export default function ProspectConvo(props: { prospects: Prospect[] }) {
         style={{
           height: `calc((${INBOX_PAGE_HEIGHT} - ${HEADER_HEIGHT}px)*0.70)`,
           alignItems: 'stretch',
+          position: 'relative',
         }}
       >
+        {ai_disabled && <Badge variant="filled" sx={{ position: 'absolute', top: 10, right: 10, zIndex: 100 }} color="red">AI Disabled</Badge>}
         <ScrollArea h={`calc((${INBOX_PAGE_HEIGHT} - ${HEADER_HEIGHT}px)*0.70)`} viewportRef={viewport}>
           <div style={{ marginTop: 10, marginBottom: 10 }}>
             <LoadingOverlay loader={loaderWithText('')} visible={isFetchingMessages} />
