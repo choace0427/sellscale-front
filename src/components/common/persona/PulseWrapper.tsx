0fd5213afd9af60ad78c21230ca0ba505c1ebd70
Aakash Adesara <aakash.adesara@gmail.com>
2023-08-30 10:57:22 -0700

    Remove AI Filtering Tab

--- src/components/common/persona/PulseWrapper.tsx
+++ src/components/common/persona/PulseWrapper.tsx
@@ -16,26 +16,10 @@ export const PulseWrapper = () => {
 
   return (
     <Box m='md'>
-      <Tabs defaultValue="ai_filter">
-        <Tabs.List>
-          <Tabs.Tab value="ai_filter" icon={<IconRobot size="0.8rem" />}>AI Filtering</Tabs.Tab>
-          <Tabs.Tab value="rule_based_filter" icon={<IconRuler size="0.8rem" />}>Rule Based Filter</Tabs.Tab>
-        </Tabs.List>
-
-        <Tabs.Panel value="ai_filter" pt="xs">
-          <Stack>
-            <Pulse personaOverview={currentProject} />
-            <PulseBarChart />
-          </Stack>
-        </Tabs.Panel>
-
-        <Tabs.Panel value="rule_based_filter" pt="xs">
-          <iframe 
-            src={`https://sellscale.retool.com/embedded/public/d26f8f26-7d77-4827-b53e-1ae2c0eda0c8#authToken=${userToken}&archetype_id=${currentProjectId}`}
-            style={{ width: '100%', height: '1000px', border: 'none' }}
-          />
-        </Tabs.Panel>
-      </Tabs>
+      <iframe 
+          src={`https://sellscale.retool.com/embedded/public/d26f8f26-7d77-4827-b53e-1ae2c0eda0c8#authToken=${userToken}&archetype_id=${currentProjectId}`}
+          style={{ width: '100%', height: '1000px', border: 'none' }}
+        />
     </Box>
   );
 };
