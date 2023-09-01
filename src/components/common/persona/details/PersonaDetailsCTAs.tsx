b6ef6a6ff417d52d9c5fbbd89e9ceff232e90416
Aakash Adesara <aakash.adesara@gmail.com>
2023-08-31 19:25:45 -0700

    hotfix

--- src/components/common/persona/details/PersonaDetailsCTAs.tsx
+++ src/components/common/persona/details/PersonaDetailsCTAs.tsx
@@ -186,7 +186,7 @@ export default function PersonaDetailsCTAs(props: { personas?: Archetype[], onCT
                 "lime",
                 "yellow",
               ];
-              const deterministicColor = str => mantineColorOptions[Math.abs([...str].reduce((hash, char) => (hash << 5) - hash + char.charCodeAt(0), 0)) % mantineColorOptions.length];
+              const deterministicColor = (str: any) => mantineColorOptions[Math.abs([...str].reduce((hash, char) => (hash << 5) - hash + char.charCodeAt(0), 0)) % mantineColorOptions.length];
               const randomColorFromCtaType = deterministicColor(record.cta_type)
 
               return (
