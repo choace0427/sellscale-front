8c985d1343e85e425077cbb95d8a191fa410ebdc
Aakash Adesara <aakash.adesara@gmail.com>
2023-09-28 12:20:48 -0700

    Sidebar Updates

--- src/components/common/persona/ICPFilter/Sidebar.tsx
+++ src/components/common/persona/ICPFilter/Sidebar.tsx
@@ -24,6 +24,7 @@ const Sidebar: FC<{
       <SidebarHeader
         sideBarVisible={sideBarVisible}
         toggleSideBar={toggleSideBar}
+        isTesting={isTesting}
         setIsTesting={setIsTesting}
       />
 
