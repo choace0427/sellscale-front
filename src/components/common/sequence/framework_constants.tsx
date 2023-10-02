0751934c283e26d2dcba5156adb6401932626105
Aakash Adesara <aakash.adesara@gmail.com>
2023-10-02 15:14:57 -0700

    Customized Templates

--- src/components/common/sequence/framework_constants.tsx
+++ src/components/common/sequence/framework_constants.tsx
@@ -1,26 +1,26 @@
-export const BUMP_FRAMEWORK_OPTIONS: any = {
-    'role-have-to-do-with': {
-        "name": "Does your role have to do with?",
-        "raw_prompt": "Do a short follow-up acknowledging they're busy using their role description - and that we'd love to chat about any [relevant painpoints their role may face]\n\nWhich pain points would this persona have? (bullet points)\nAnswer: \n- _________________",
-        "human_readable_prompt": "Connect with their role and ask them if their role has anything to do with what you want to connect with them on",
-        "length": 'MEDIUM',
-    },
-    'short-introduction': {
-        "name": "Short Introduction",
-        "raw_prompt": "1. Introduce what [our company] does\n2. Connect with them using [their profile]\n3. Ask if they're open to chat for 15 minutes",
-        "human_readable_prompt": "Simply introduce yourself and mention how it's great to connect with them",
-        "length": 'SHORT'
-    },
-    'pain-points-opener': {
-        'name': 'Paint Points Opener',
-        'raw_prompt': '1. Thank them for connecting\n2. Connect with their role and any pain points that they have in their role based on what our company does\n3. How is it going with pain points they my face in their role?\n\nWhat pain points would this persona have? (bullet points)\nAnswer: \n- _________________',
-        'human_readable_prompt': 'Tap into a potential pain point that they may have in their role and connect with them on that',
-        'length': 'MEDIUM'
-    },
-    'any-other-person': {
-        'name': 'Any Other Persona?',
-        'raw_prompt': "Ask if there's another person better to connect regarding the pain points our product solves.",
-        'human_readable_prompt': 'Ask if there is another person better to connect with regarding the pain points our product solves',
-        'length': 'MEDIUM'
-    }
-}
+// export const BUMP_FRAMEWORK_OPTIONS: any = {
+//     'role-have-to-do-with': {
+//         "name": "Does your role have to do with?",
+//         "raw_prompt": "Do a short follow-up acknowledging they're busy using their role description - and that we'd love to chat about any [relevant painpoints their role may face]\n\nWhich pain points would this persona have? (bullet points)\nAnswer: \n- _________________",
+//         "human_readable_prompt": "Connect with their role and ask them if their role has anything to do with what you want to connect with them on",
+//         "length": 'MEDIUM',
+//     },
+//     'short-introduction': {
+//         "name": "Short Introduction",
+//         "raw_prompt": "1. Introduce what [our company] does\n2. Connect with them using [their profile]\n3. Ask if they're open to chat for 15 minutes",
+//         "human_readable_prompt": "Simply introduce yourself and mention how it's great to connect with them",
+//         "length": 'SHORT'
+//     },
+//     'pain-points-opener': {
+//         'name': 'Paint Points Opener',
+//         'raw_prompt': '1. Thank them for connecting\n2. Connect with their role and any pain points that they have in their role based on what our company does\n3. How is it going with pain points they my face in their role?\n\nWhat pain points would this persona have? (bullet points)\nAnswer: \n- _________________',
+//         'human_readable_prompt': 'Tap into a potential pain point that they may have in their role and connect with them on that',
+//         'length': 'MEDIUM'
+//     },
+//     'any-other-person': {
+//         'name': 'Any Other Persona?',
+//         'raw_prompt': "Ask if there's another person better to connect regarding the pain points our product solves.",
+//         'human_readable_prompt': 'Ask if there is another person better to connect with regarding the pain points our product solves',
+//         'length': 'MEDIUM'
+//     }
+// }
