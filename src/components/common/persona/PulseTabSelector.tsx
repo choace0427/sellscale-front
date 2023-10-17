f9a60d7505cb613af4d9399768693d52046979bf
Aakash Adesara <aakash.adesara@gmail.com>
2023-10-17 13:55:45 -0700

    Old ICP View is Back

--- src/components/common/persona/PulseTabSelector.tsx
+++ src/components/common/persona/PulseTabSelector.tsx
@@ -28,7 +28,19 @@ const PulseTabSelector = () => {
   }, []);
 
   return (
-    <ICPFilters />
+    <Tabs defaultValue="new_view">
+      <Tabs.List>
+        <Tabs.Tab value="new_view">New View</Tabs.Tab>
+        <Tabs.Tab value="old_view">Old View</Tabs.Tab>
+      </Tabs.List>
+
+      <Tabs.Panel value="old_view">
+        <PulseWrapper />
+      </Tabs.Panel>
+      <Tabs.Panel value="new_view">
+        <ICPFilters />
+      </Tabs.Panel>
+    </Tabs>
   );
 };
 
