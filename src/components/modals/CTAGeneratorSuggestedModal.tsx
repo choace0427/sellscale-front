f2b4573a12fcc4dbc744c1bf8366979b3381d1fe
mykolapetrovsk <mykolapetrovsk@gmail.com>
2023-10-21 19:02:45 +0700

    Add functionality to the AI CTA generator component

--- src/components/modals/CTAGeneratorSuggestedModal.tsx
+++ src/components/modals/CTAGeneratorSuggestedModal.tsx
@@ -1,3 +1,6 @@
+import { currentProjectState } from "@atoms/personaAtoms";
+import { userTokenState } from "@atoms/userAtoms";
+import { CTA } from "@common/sequence/CTAGenerator";
 import {
   Modal,
   Divider,
@@ -8,53 +11,90 @@ import {
   useMantineTheme,
   Card,
   Badge,
+  Input,
+  Textarea,
+  LoadingOverlay,
+  TextInput,
+  Box,
 } from "@mantine/core";
-import { IconEdit, IconPencil, IconX } from "@tabler/icons";
-
-export interface GenerateTabOption {
-  id: number;
-  label: string;
-  types: string[];
-}
-
-const options: GenerateTabOption[] = [
-  {
-    id: 1,
-    label:
-      "Are you interested in building with GPT + LLM technology? SellScale is the perfect place to do that.",
-    types: ["Problem Based", "View Example"],
-  },
-  {
-    id: 2,
-    label:
-      "Are you interested in building with GPT + LLM technology? SellScale is the perfect place to do that.",
-    types: ["Problem Based", "View Example"],
-  },
-  {
-    id: 3,
-    label:
-      "Are you interested in building with GPT + LLM technology? SellScale is the perfect place to do that.",
-    types: ["Problem Based", "View Example"],
-  },
-  {
-    id: 4,
-    label:
-      "Are you interested in building with GPT + LLM technology? SellScale is the perfect place to do that.",
-    types: ["Problem Based", "View Example"],
-  },
-  {
-    id: 5,
-    label:
-      "Are you interested in building with GPT + LLM technology? SellScale is the perfect place to do that.",
-    types: ["Problem Based", "View Example"],
-  },
-];
+import { useForm } from "@mantine/form";
+import { showNotification } from "@mantine/notifications";
+import { IconCheck, IconEdit, IconPencil, IconX } from "@tabler/icons";
+import { useQueryClient } from "@tanstack/react-query";
+import createCTA from "@utils/requests/createCTA";
+import { generateCTAs } from "@utils/requests/generateCTAs";
+import { useEffect, useState } from "react";
+import { useRecoilValue } from "recoil";
 
 export const CTAGeneratorSuggestedModal: React.FC<{
+  generatedCTAs: CTA[];
   modalOpened: boolean;
   closeModal: () => void;
-}> = ({ modalOpened, closeModal }) => {
+  formValue: {
+    company: string;
+    persona: string;
+    proposition: string;
+  };
+}> = ({ generatedCTAs, modalOpened, closeModal, formValue }) => {
   const theme = useMantineTheme();
+  const [isLoading, setLoading] = useState(false);
+  const [CTAs, setCTAs] = useState<CTA[]>([]);
+  const userToken = useRecoilValue(userTokenState);
+
+  const [isShowingCompanyInput, setShowingCompanyInput] = useState(false);
+  const [isShowingPersonaInput, setShowingPersonaInput] = useState(false);
+  const [
+    isShowingValueAddedPropositionInput,
+    setShowingValueAddedPropositionInput,
+  ] = useState(false);
+
+  useEffect(() => {
+    setCTAs(generatedCTAs);
+  }, [generatedCTAs]);
+
+  useEffect(() => {
+    form.setValues(formValue);
+  }, [formValue]);
+
+  const form = useForm({
+    initialValues: {
+      company: formValue.company,
+      persona: formValue.persona,
+      proposition: `${formValue.proposition}`,
+    },
+  });
+
+
+  const onCloseModal= () => {
+    setCTAs([])
+    setShowingCompanyInput(false);
+    setShowingPersonaInput(false);
+    setShowingValueAddedPropositionInput(false);
+    form.reset();
+    closeModal()
+  }
+
+  const handleReGenerateCTA = async (values: typeof form.values) => {
+    setLoading(true);
+    const result = await generateCTAs(
+      userToken,
+      values.company,
+      values.persona,
+      values.proposition
+    );
+    setLoading(false);
+    if (result.status === "success") {
+      setCTAs(result.data.map((cta: any) => ({ ...cta, enabled: true })));
+    } else {
+      showNotification({
+        id: "generate-cta-ideas-error",
+        title: "Generation Failed",
+        message: "Failed to generate CTA ideas. Please try again!",
+        color: "red",
+        autoClose: 5000,
+      });
+    }
+  };
 
   return (
     <Modal.Root
@@ -65,6 +105,8 @@ export const CTAGeneratorSuggestedModal: React.FC<{
     >
       <Modal.Overlay blur={3} color="gray.2" opacity={0.5} />
       <Modal.Content sx={{ borderRadius: "8px" }}>
+        <LoadingOverlay visible={isLoading} overlayBlur={2} />
+
         <Modal.Header
           md-px={"1.5rem"}
           px={"1rem"}
@@ -85,7 +127,7 @@ export const CTAGeneratorSuggestedModal: React.FC<{
           <ActionIcon
             variant="outline"
             size={"sm"}
-            onClick={closeModal}
+            onClick={onCloseModal}
             sx={{ borderColor: "#E9ECEF", borderRadius: 999 }}
           >
             <IconX color="#FFFFFF" />
@@ -93,68 +135,150 @@ export const CTAGeneratorSuggestedModal: React.FC<{
         </Modal.Header>
 
         <Modal.Body p={"1rem"}>
-          <Flex
-            wrap={"wrap"}
-            align={"flex-start"}
-            justify={"start"}
-            sx={{
-              border: "1px solid #E9ECEF",
-              borderRadius: "0.5rem",
-            }}
-            p="sm"
-            my={"1rem"}
-            gap={"0.5rem"}
-          >
-            <Button
-              radius={"xl"}
-              variant="light"
-              color="blue"
-              h={"2.25rem"}
-              rightIcon={<IconPencil size={"1rem"} />}
-            >
-              <Text color="black">Add glow</Text>
-            </Button>
+          <form onSubmit={form.onSubmit(handleReGenerateCTA)}>
+            <>
+              <Box>
+                <Flex
+                  wrap={"wrap"}
+                  align={"center"}
+                  justify={"start"}
+                  sx={{
+                    border: "1px solid #E9ECEF",
+                    borderRadius: "0.5rem",
+                  }}
+                  p="sm"
+                  my={"1rem"}
+                  gap={"0.5rem"}
+                >
+                  {isShowingCompanyInput ? (
+                    <TextInput
+                      withAsterisk
+                      placeholder="Company"
+                      size="xs"
+                      radius="xl"
+                      required
+                      {...form.getInputProps("company")}
+                      rightSection={
+                        <IconCheck
+                          cursor={"pointer"}
+                          size={"1rem"}
+                          onClick={() => setShowingCompanyInput(false)}
+                        />
+                      }
+                    />
+                  ) : (
+                    <Button
+                      radius={"xl"}
+                      variant="light"
+                      color="blue"
+                      h={"2.25rem"}
+                      rightIcon={<IconPencil size={"1rem"} />}
+                      onClick={() => setShowingCompanyInput(true)}
+                    >
+                      <Text color="black">
+                        {form.values.company?.length
+                          ? form.values.company
+                          : "Company"}
+                      </Text>
+                    </Button>
+                  )}
 
-            <Flex h={"2.25rem"} align={"center"}>
-              <Text>helps</Text>
-            </Flex>
+                  <Flex h={"2.25rem"} align={"center"}>
+                    <Text>helps</Text>
+                  </Flex>
 
-            <Button
-              radius={"xl"}
-              variant="light"
-              color="blue"
-              h={"2.25rem"}
-              rightIcon={<IconPencil size={"1rem"} />}
-            >
-              <Text color="black">E-Commerce leaders</Text>
-            </Button>
+                  {isShowingPersonaInput ? (
+                    <TextInput
+                      withAsterisk
+                      placeholder="Persona"
+                      size="xs"
+                      radius="xl"
+                      required
+                      {...form.getInputProps("persona")}
+                      rightSection={
+                        <IconCheck
+                          cursor={"pointer"}
+                          size={"1rem"}
+                          onClick={() => setShowingPersonaInput(false)}
+                        />
+                      }
+                    />
+                  ) : (
+                    <Button
+                      radius={"xl"}
+                      variant="light"
+                      color="blue"
+                      h={"1.5rem"}
+                      rightIcon={<IconPencil size={"1rem"} />}
+                      onClick={() => setShowingPersonaInput(true)}
+                    >
+                      <Text color="black">
+                        {form.values.persona?.length
+                          ? form.values.persona
+                          : "Persona"}
+                      </Text>
+                    </Button>
+                  )}
 
-            <Flex h={"2.25rem"} align={"center"}>
-              <Text>with</Text>
-            </Flex>
+                  <Flex h={"2.25rem"} align={"center"}>
+                    <Text>with</Text>
+                  </Flex>
 
-            <Button
-              radius={"xl"}
-              variant="light"
-              color="blue"
-              h={"2.25rem"}
-              rightIcon={<IconPencil size={"1rem"} />}
-            >
-              <Text color="black">engaging their</Text>
-            </Button>
-          </Flex>
+                  {isShowingValueAddedPropositionInput ? (
+                    <TextInput
+                      withAsterisk
+                      placeholder="Value Added Proposition"
+                      size="xs"
+                      radius="xl"
+                      required
+                      {...form.getInputProps("proposition")}
+                      rightSection={
+                        <IconCheck
+                          cursor={"pointer"}
+                          size={"1rem"}
+                          onClick={() =>
+                            setShowingValueAddedPropositionInput(false)
+                          }
+                        />
+                      }
+                    />
+                  ) : (
+                    <Button
+                      radius={"xl"}
+                      variant="light"
+                      color="blue"
+                      h={"1.5rem"}
+                      rightIcon={<IconPencil size={"1rem"} />}
+                      onClick={() => setShowingValueAddedPropositionInput(true)}
+                    >
+                      <Text color="black">
+                        {form.values.proposition?.length
+                          ? form.values.proposition
+                          : "Value Added Proposition"}
+                      </Text>
+                    </Button>
+                  )}
+                </Flex>
+              </Box>
 
-          <Flex justify={"center"} mb={"2rem"}>
-            <Button
-              radius={"xl"}
-              variant="filled"
-              color="blue"
-              h={"2.25rem"}
-              onClick={() => {}}
-            >
-              Re-Generate CTA
-            </Button>
-          </Flex>
+              <Flex justify={"center"} mb={"2rem"}>
+                <Button
+                  radius={"xl"}
+                  variant="filled"
+                  color="blue"
+                  h={"2.25rem"}
+                  type="submit"
+                  disabled={
+                    !form.values.company.length ||
+                    !form.values.persona.length ||
+                    !form.values.proposition.length
+                  }
+                >
+                  Re-Generate CTA
+                </Button>
+              </Flex>
+            </>
+          </form>
 
           <Divider
             my="xl"
@@ -164,15 +288,30 @@ export const CTAGeneratorSuggestedModal: React.FC<{
             labelPosition="center"
             label={
               <Text fw={700} color="gray.5" size={"sm"}>
-                Suggested CTAs ({options.length})
+                Suggested CTAs ({CTAs.length})
               </Text>
             }
           />
 
           <Flex direction={"column"} gap={"1rem"}>
-            {options.map((e, index) => (
-              <CTASuggestOption data={e} key={index} />
-            ))}
+            {CTAs.map((e, index) => {
+              const onUseCTASuccess = () => {
+                const newCTAs = [...CTAs];
+                newCTAs[index] = {
+                  enabled: false,
+                  tag: e.tag,
+                  cta: e.cta,
+                };
+                setCTAs(newCTAs);
+              };
+              return (
+                <CTASuggestOption
+                  data={e}
+                  key={index}
+                  onUseCTASuccess={onUseCTASuccess}
+                />
+              );
+            })}
           </Flex>
         </Modal.Body>
       </Modal.Content>
@@ -181,9 +320,78 @@ export const CTAGeneratorSuggestedModal: React.FC<{
 };
 
 const CTASuggestOption: React.FC<{
-  data: GenerateTabOption;
-  onClickEdit?: () => void;
-}> = ({ data, onClickEdit }) => {
+  data: CTA;
+  onUseCTASuccess: () => void;
+}> = ({ data, onUseCTASuccess }) => {
+  const userToken = useRecoilValue(userTokenState);
+  const queryClient = useQueryClient();
+  const [isLoading, setLoading] = useState(false);
+  const [isEditing, setEditing] = useState(false);
+  const [CTAValue, setCTAValue] = useState("");
+  const currentProject = useRecoilValue(currentProjectState);
+
+  useEffect(() => {
+    setCTAValue(data.cta);
+  }, [data.cta]);
+
+  const handleUseCTA = async () => {
+    if (!currentProject) {
+      showNotification({
+        id: "create-cta-error",
+        title: "Error",
+        message: "Current project not found",
+        color: "red",
+        autoClose: 2000,
+      });
+      return;
+    }
+    if (CTAValue.length > 120) {
+      showNotification({
+        id: "create-cta-error",
+        title: "Error",
+        message: "CTA must be less than 120 characters",
+        color: "red",
+        autoClose: 2000,
+      });
+      return;
+    }
+
+    setLoading(true);
+    setEditing(false);
+    // Create the CTA
+
+    const response = await createCTA(
+      userToken,
+      currentProject.id,
+      CTAValue,
+      undefined,
+      data.tag.replace("[", "") + "-based"
+    );
+    queryClient.invalidateQueries({
+      queryKey: [`query-cta-data-${currentProject.id}`],
+    });
+    setLoading(false);
+    if (response.status === "success") {
+      onUseCTASuccess();
+
+      showNotification({
+        id: "create-cta-success",
+        title: "Success",
+        message: `Added CTA to Persona`,
+        color: "green",
+        autoClose: 5000,
+      });
+    } else {
+      showNotification({
+        id: "create-cta-error",
+        title: "Error",
+        message: "Failed to add CTA to Persona",
+        color: "red",
+        autoClose: false,
+      });
+    }
+  };
+
   return (
     <Card
       radius={"md"}
@@ -195,38 +403,59 @@ const CTASuggestOption: React.FC<{
       })}
     >
       <Flex pos={"absolute"} top={-10} gap={"0.5rem"}>
-        {data.types.map((e) => (
-          <Badge
-            fw={700}
-            color={"green"}
-            variant="light"
-            style={{ zIndex: 10 }}
-          >
-            {e ? e.replace("-", " ") : "CTA Type"}
-          </Badge>
-        ))}
+        <Badge fw={700} color={"green"} variant="light" style={{ zIndex: 10 }}>
+          {(data.tag || "Tag").replace("-", " ")}
+        </Badge>
       </Flex>
 
       <Flex direction={"row"} w={"100%"} gap={"0.75rem"}>
         <Flex wrap={"wrap"} gap={"0.5rem"} align={"center"}></Flex>
-        <Flex align={"center"}>
-          <Text color={"gray.8"} fw={500}>
-            {data.label}
-            <Button
-              size="compact-xs"
-              variant="light"
-              leftIcon={<IconEdit size={14} />}
-              fz="xs"
-              radius="lg"
-              ml={"0.25rem"}
-              onClick={onClickEdit}
-            >
-              Edit CTA
-            </Button>
-          </Text>
+        <Flex align={"center"} style={{ flex: 1 }}>
+          {isEditing ? (
+            <Textarea
+              placeholder="Clearable input"
+              value={CTAValue}
+              onChange={(event) => setCTAValue(event.currentTarget.value)}
+              style={{
+                width: "100%",
+              }}
+              rightSection={
+                <IconCheck
+                  cursor={"pointer"}
+                  size={"1rem"}
+                  onClick={() => setEditing(false)}
+                />
+              }
+            />
+          ) : (
+            <Text color={"gray.8"} fw={500}>
+              {CTAValue}
+              <Button
+                size="compact-xs"
+                variant="light"
+                leftIcon={<IconEdit size={14} />}
+                fz="xs"
+                radius="lg"
+                ml={"0.25rem"}
+                disabled={!data.enabled}
+                onClick={() => setEditing(true)}
+              >
+                Edit CTA
+              </Button>
+            </Text>
+          )}
         </Flex>
         <Flex gap={"0.5rem"} align={"center"} ml={"auto"}>
-          <Button size="sm" variant="light" fz="sm" radius="lg" fw={700}>
+          <Button
+            disabled={!data.enabled}
+            loading={isLoading}
+            size="sm"
+            variant="light"
+            fz="sm"
+            radius="lg"
+            fw={700}
+            onClick={handleUseCTA}
+          >
             Use CTA
           </Button>
         </Flex>
