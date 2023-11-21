2f55735a5f34be32004d6cf1776c580e1216fc7b
Aakash Adesara <aakash.adesara@gmail.com>
2023-11-21 14:50:33 -0800

    Prospects Found

--- src/components/pages/TriggersList.tsx
+++ src/components/pages/TriggersList.tsx
@@ -1,17 +1,232 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Card, Text, Badge, Switch, Grid, Box, Title, Flex, Loader} from '@mantine/core';
+import { Button, Card, Text, Badge, Switch, Grid, Box, Title, Flex, Loader, Collapse, Divider, Modal, Table, LoadingOverlay} from '@mantine/core';
 import { useNavigate } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
 import { userTokenState } from '@atoms/userAtoms';
 import { API_URL } from '@constants/data';
 import PageFrame from '@common/PageFrame';
-import { IconEdit, IconPlus } from '@tabler/icons';
+import { IconCheck, IconChevronsDown, IconEdit, IconEngine, IconPlus, IconRefresh, IconSquaresDiagonal } from '@tabler/icons';
+import { useDisclosure } from '@mantine/hooks';
+import moment from 'moment';
+import { DataSheetGrid, textColumn, keyColumn } from 'react-datasheet-grid';
+import 'react-datasheet-grid/dist/style.css';
+import { deterministicMantineColor } from '@utils/requests/utils';
+import { IconChevronCompactDown } from '@tabler/icons-react';
+import { List } from 'lodash';
+
+export interface TriggerRun {
+  id: number;
+  run_at: string;
+  completed_at: string;
+  run_status: string;
+  num_prospects: number;
+  companies: string[];
+}
+
+export interface TriggerRunProspect {
+  first_name: string;
+  last_name: string;
+  title: string;
+  company: string;
+  linkedin_url: string;
+  custom_data: string;
+}
+
+const RecentRuns = ({ trigger_id, userToken }: any) => {
+  const [runs, setRuns] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [selectedRun, setSelectedRun]: any = useState(null);
+  const [prospects, setProspects] = useState([]);
+  const [prospectsData, setProspectsData]: any = useState([]);
+  const [isProspectsModalOpen, setIsProspectsModalOpen] = useState(false);
+
+  const fetchRuns = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${API_URL}/triggers/trigger/get_runs/${trigger_id}`, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${userToken}`,
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setRuns(data.trigger_runs);
+    } catch (error) {
+      console.error('Error fetching trigger runs:', error);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchRuns();
+  }, [trigger_id, userToken]);
+
+  const handleRowClick = async (run: TriggerRun) => {
+    setSelectedRun(run);
+    setIsProspectsModalOpen(true);
+    try {
+      const response = await fetch(`${API_URL}/triggers/trigger/get_prospects/${run.id}`, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${userToken}`,
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      const formattedProspects = data.trigger_prospects.map((prospect: TriggerRunProspect) => ({
+        first_name: prospect.first_name,
+        last_name: prospect.last_name,
+        title: prospect.title,
+        company: prospect.company,
+        linkedin_url: prospect.linkedin_url,
+        custom_data: prospect.custom_data,
+      }));
+
+      setProspectsData(formattedProspects);
+    } catch (error) {
+      console.error('Error fetching prospects:', error);
+    }
+  };
+
+  const columns = [
+    { ...keyColumn('first_name', textColumn), title: 'First Name' },
+    { ...keyColumn('last_name', textColumn), title: 'Last Name' },
+    { ...keyColumn('title', textColumn), title: 'Title' },
+    { ...keyColumn('company', textColumn), title: 'Company' },
+    { ...keyColumn('linkedin_url', textColumn), title: 'LinkedIn URL' },
+    { ...keyColumn('custom_data', textColumn), title: 'Custom Data' },
+  ];
+
+
+  const rows = runs.map((run: TriggerRun) => (
+    <tr key={run.id} onClick={() => handleRowClick(run)}>
+      <td>{new Date(run.run_at).toLocaleString()}</td>
+      <td>{run.completed_at ? new Date(run.completed_at).toLocaleString() : 'Running'}</td>
+      <td><Badge 
+        leftSection={run.run_status === 'Running' ? <IconEngine size={16} /> : run.run_status == 'Completed' ? <IconCheck size={16} /> : <IconSquaresDiagonal size={16} />}
+        color={run.run_status === 'Running' ? 'blue' : run.run_status == 'Completed' ? 'green' : 'yellow'}>
+        {run.run_status}
+      </Badge></td>
+      <td>{run.num_prospects || '-'}</td>
+      <td>
+        <Flex>
+          {run.companies.slice(0, 2).map(
+            (company: string) => <Badge mt='2px' color={deterministicMantineColor(company)} size='xs'>{company}</Badge>)
+          } 
+          {run.companies.length > 3 && <Text fz='xs' ml='xs'>{'and ' + (run.companies.length - 2) + ' more'}</Text>}
+        </Flex>
+      </td>
+      <Button compact size='xs' onClick={() => setIsProspectsModalOpen(true)} disabled={run.num_prospects === 0}>
+        {run.num_prospects === 0 ? 'No prospects found' : 'View ' + run.num_prospects + ' prospects'}
+      </Button>
+    </tr>
+  ));
+
+  const convertToCSV = (headerArr: any, objArray: any) => {
+    const array = typeof objArray !== 'object' ? JSON.parse(objArray) : objArray;
+    let str = '';
+
+    // add headers
+    for (let index in headerArr) {
+      str += headerArr[index] + ',';
+    }
+
+    for (let i = 0; i < array.length; i++) {
+      let line = '';
+      for (let index in array[i]) {
+        if (line !== '') line += ',';
+
+        line += array[i][index];
+      }
+
+      str += line + '\r\n';
+    }
+
+    return str;
+  }
+
+  const downloadCSV = () => {
+    const headerArr = ['first_name', 'last_name', 'title', 'company', 'linkedin_url', 'custom_data'];
+    const csvData = convertToCSV(headerArr, prospectsData);
+    const blob = new Blob([csvData], { type: 'text/csv;charset=utf-8;' });
+    const link = document.createElement("a");
+    const url = URL.createObjectURL(blob);
+    link.setAttribute("href", url);
+    link.setAttribute("download", `prospects_run_${selectedRun?.id}.csv`);
+    link.style.visibility = 'hidden';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  return (
+    <>
+      <Box>
+        <Box w='100%' sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end' }}>
+          <Button style={{ marginBottom: '10px' }} ml='auto' variant='subtle' compact mt='md' onClick={() => fetchRuns()}>
+            <IconRefresh size={16} />
+          </Button>
+        </Box>
+        <LoadingOverlay visible={loading}/>
+        <Table striped highlightOnHover>
+          <thead>
+            <tr>
+              <th>Start Time</th>
+              <th>End Time</th>
+              <th>Status</th>
+              <th># Prospects Found</th>
+              <th>Companies</th>
+              <th>
+                
+              </th>
+            </tr>
+          </thead>
+          <tbody>{rows}</tbody>
+        </Table>
+        
+      </Box>
+
+      <Modal
+        opened={isProspectsModalOpen}
+        onClose={() => setIsProspectsModalOpen(false)}
+        title={`Found ${prospectsData.length} prospects`}
+        size="90%"
+      >
+        <DataSheetGrid
+          height={500}
+          value={prospectsData}
+          onChange={setProspectsData}
+          columns={columns}
+        />
+        <Box w='100%' sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end' }}>
+          <Button onClick={downloadCSV} style={{ marginBottom: '10px' }} ml='auto' color='orange' compact mt='md'>
+            Download as CSV
+          </Button>
+        </Box>
+      </Modal>
+    </>
+  );
+};
+
 
 const TriggersList = () => {
   const [triggers, setTriggers] = useState([]);
   const [userToken] = useRecoilState(userTokenState);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+
+  const [currentOpenTrigger, setCurrentOpenTrigger] = useState(-1);
 
   useEffect(() => {
     const fetchTriggers = async () => {
@@ -84,39 +299,76 @@ const TriggersList = () => {
       }
 
       <Grid mt='md' w='100%'>
-        {triggers.map((trigger: any) => (
-          <Grid.Col span={12} key={trigger.id} mb='xs'>
-            <Card shadow="sm" p="lg">
-              <Grid justify="space-between" align="center">
-                <Grid.Col span={6}>
-                  <Box>
+        {triggers.map((trigger: any) => {
+          return <Box w='100%' key={trigger.id}>
+              <Card shadow="sm" p="lg">
+                <Grid justify="space-between" align="center">
+                  <Grid.Col span={6}>
                     <Box>
-                      <Text fw='bold' size="lg">{trigger.emoji} {trigger.name}</Text>
+                      <Badge size='xs' color="blue">{trigger.trigger_type.replaceAll("_", " ")}</Badge>
+                      <Box>
+                        <Text fw='bold' size="lg">{trigger.emoji} {trigger.name}</Text>
+                      </Box>
+                      <Text fz='xs' color='black'>{trigger.description}</Text>
+                      <Text mt='xs' color='gray' size='xs'><b>Last run:</b> {moment(trigger.last_run).format('MMM Do YYYY')} | <b>Next run:</b> {moment(trigger.next_run).format('MMM Do YYYY')}</Text>
                     </Box>
-                    <Text fz='xs' color='black'>{trigger.description}</Text>
-                    <Text mt='xs' color='gray' size='xs'><b>Last run:</b> {trigger.last_run} | <b>Next run:</b> {trigger.next_run}</Text>
-                  </Box>
-                </Grid.Col>
+                  </Grid.Col>
 
-                <Grid.Col span={1}>
-                </Grid.Col>
+                  <Grid.Col span={2}>
+                    <Box>
+                      <Flex sx={{textAlign: 'center'}}>
+                        <Box>
+                          <Text fw='bold' align='center'>
+                            132 
+                          </Text>
+                          <Text fz='xs' color='gray' align='center'>
+                            sourced
+                          </Text>
+                        </Box>
+                        <Box ml='xl'>
+                          <Text fw='bold' align='center'>
+                            84 
+                          </Text>
+                          <Text fz='xs' color='gray' align='center'>
+                            queued
+                          </Text>
+                        </Box>
+                      </Flex>
+                      <Button mt='xs' compact size='xs' leftIcon={<IconEdit size={16} />} onClick={() => navigate(`/setup/linkedin?campaign_id=${trigger.client_archetype_id}`)}>
+                        Edit Campaign
+                      </Button>
+                    </Box>
+                  </Grid.Col>
 
-                <Grid.Col span={2}>
-                  <Button size='xs' leftIcon={<IconEdit size={16} />} onClick={() => navigate(`/setup/linkedin?campaign_id=${trigger.client_archetype_id}`)}>
-                    Edit Campaign
-                  </Button>
-                </Grid.Col>
+                  <Grid.Col span={1}>
+                    <Switch defaultChecked={trigger.active} label="Active" color="green" />
+                  </Grid.Col>
 
-                <Grid.Col span={2}>
-                  <Badge size='lg' color="blue">{trigger.trigger_type.replaceAll("_", " ")}</Badge>
-                </Grid.Col>
-                <Grid.Col span={1}>
-                  <Switch defaultChecked={trigger.active} label="Active" color="green" />
-                </Grid.Col>
-              </Grid>
-            </Card>
-          </Grid.Col>
-        ))}
+                  <Grid.Col span={2}>
+                    <Button
+                      variant='outline'
+                      onClick={() => {
+                        if (currentOpenTrigger === trigger.id) {
+                          setCurrentOpenTrigger(-1);
+                        } else {
+                          setCurrentOpenTrigger(trigger.id);
+                        }
+                      }}
+                      rightIcon={currentOpenTrigger === trigger.id ? <IconChevronsDown size={16} style={{ transform: 'rotate(-180deg)' }} /> : <IconChevronsDown size={16} />}
+                    >
+                      View Runs
+                    </Button>
+                  </Grid.Col>
+                </Grid>
+
+                <Collapse in={currentOpenTrigger === trigger.id}>
+                  <Card withBorder mt='md' p='md'>
+                    <RecentRuns trigger_id={trigger.id} userToken={userToken} />
+                  </Card>
+                </Collapse>
+              </Card>
+          </Box>
+        })}
       </Grid>
     </PageFrame>
   );
