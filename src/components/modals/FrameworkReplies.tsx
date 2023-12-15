172258243fb64e8e44f18c62a4f3d8aed66346a8
dwatklnsweb <94058928+dwatklnsweb@users.noreply.github.com>
2023-12-15 14:16:20 -0600

    minor fix

--- src/components/modals/FrameworkReplies.tsx
+++ src/components/modals/FrameworkReplies.tsx
@@ -1,77 +1,44 @@
+import { userTokenState } from '@atoms/userAtoms';
 import {
   Text,
   Paper,
   useMantineTheme,
-  ScrollArea,
-  Avatar,
   Box,
-  Flex
+  ScrollArea,
+  LoadingOverlay,
+  Avatar,
+  Flex,
 } from '@mantine/core';
 import { ContextModalProps } from '@mantine/modals';
+import { useQuery } from '@tanstack/react-query';
+import { useRecoilValue } from 'recoil';
+import { getBumpFrameworkMessages } from "@utils/requests/getBumpFrameworks";
+
 
 export default function FrameworkReplies({
   innerProps,
-}: ContextModalProps<{ prospectId: number; threadId: string }>) {
+}: ContextModalProps<{ bumpId: number }>) {
   const theme = useMantineTheme();
+  const userToken = useRecoilValue(userTokenState);
 
-  /* fake data */
-  const data = [
-    {
-      full_name: "Liz Rivera, MSEd",
-      img_url: "https://media.licdn.com/dms/image/D5603AQHlnIz2kuxLZQ/profile-displayphoto-shrink_800_800/0/1697738506774?e=1707955200&v=beta&t=PigxEnbwE2Sn1chrx_Afw_q0MvCeMhPjtVpbH1I1Tpw",
-      message: "Thanks for accepting my invite, Liz. Given your experience with managing My Wellness Check documents and coordinating clinical programs at Sylvester Comprehensive Cancer Center, I believe our mission at Simple HealthKit to deliver scalable and affordable testing programs could align well with your work. Would you be free for a 15-minute chat next week to discuss this further?",
-      created_at: "Jan 1st, 2023"
+
+  const { data, isFetching, refetch } = useQuery({
+    queryKey: [`query-framework-email-messages-${innerProps.bumpId}`],
+    queryFn: async () => {
+      const response = await getBumpFrameworkMessages(userToken, innerProps.bumpId);
+      return response.status === 'success' ? response.data : [];
     },
-    {
-      full_name: "Liz Rivera, MSEd",
-      img_url: "https://media.licdn.com/dms/image/D4E03AQGLYlbY-sTEHw/profile-displayphoto-shrink_400_400/0/1676396253631?e=1703721600&v=beta&t=bfpW2ackeOYLoc5KD3eBmrkozvX0YDkBnljFYsdn3xU",
-      message: "Thanks for accepting my invite, Liz. Given your experience with managing My Wellness Check documents and coordinating clinical programs at Sylvester Comprehensive Cancer Center, I believe our mission at Simple HealthKit to deliver scalable and affordable testing programs could align well with your work. Would you be free for a 15-minute chat next week to discuss this further?",
-      created_at: "Jan 1st, 2023"
-    },
-    {
-      full_name: "Liz Rivera, MSEd",
-      img_url: "https://media.licdn.com/dms/image/D4E03AQGLYlbY-sTEHw/profile-displayphoto-shrink_400_400/0/1676396253631?e=1703721600&v=beta&t=bfpW2ackeOYLoc5KD3eBmrkozvX0YDkBnljFYsdn3xU",
-      message: "Thanks for accepting my invite, Liz. Given your experience with managing My Wellness Check documents and coordinating clinical programs at Sylvester Comprehensive Cancer Center, I believe our mission at Simple HealthKit to deliver scalable and affordable testing programs could align well with your work. Would you be free for a 15-minute chat next week to discuss this further?",
-      created_at: "Jan 1st, 2023"
-    },
-    {
-      full_name: "Liz Rivera, MSEd",
-      img_url: "https://media.licdn.com/dms/image/D4E03AQGLYlbY-sTEHw/profile-displayphoto-shrink_400_400/0/1676396253631?e=1703721600&v=beta&t=bfpW2ackeOYLoc5KD3eBmrkozvX0YDkBnljFYsdn3xU",
-      message: "Thanks for accepting my invite, Liz. Given your experience with managing My Wellness Check documents and coordinating clinical programs at Sylvester Comprehensive Cancer Center, I believe our mission at Simple HealthKit to deliver scalable and affordable testing programs could align well with your work. Would you be free for a 15-minute chat next week to discuss this further?",
-      created_at: "Jan 1st, 2023"
-    },
-    {
-      full_name: "Liz Rivera, MSEd",
-      img_url: "https://media.licdn.com/dms/image/D4E03AQGLYlbY-sTEHw/profile-displayphoto-shrink_400_400/0/1676396253631?e=1703721600&v=beta&t=bfpW2ackeOYLoc5KD3eBmrkozvX0YDkBnljFYsdn3xU",
-      message: "Thanks for accepting my invite, Liz. Given your experience with managing My Wellness Check documents and coordinating clinical programs at Sylvester Comprehensive Cancer Center, I believe our mission at Simple HealthKit to deliver scalable and affordable testing programs could align well with your work. Would you be free for a 15-minute chat next week to discuss this further?",
-      created_at: "Jan 1st, 2023"
-    },
-    {
-      full_name: "Liz Rivera, MSEd",
-      img_url: "https://media.licdn.com/dms/image/D4E03AQGLYlbY-sTEHw/profile-displayphoto-shrink_400_400/0/1676396253631?e=1703721600&v=beta&t=bfpW2ackeOYLoc5KD3eBmrkozvX0YDkBnljFYsdn3xU",
-      message: "Thanks for accepting my invite, Liz. Given your experience with managing My Wellness Check documents and coordinating clinical programs at Sylvester Comprehensive Cancer Center, I believe our mission at Simple HealthKit to deliver scalable and affordable testing programs could align well with your work. Would you be free for a 15-minute chat next week to discuss this further?",
-      created_at: "Jan 1st, 2023"
-    },
-    {
-      full_name: "Liz Rivera, MSEd",
-      img_url: "https://media.licdn.com/dms/image/D4E03AQGLYlbY-sTEHw/profile-displayphoto-shrink_400_400/0/1676396253631?e=1703721600&v=beta&t=bfpW2ackeOYLoc5KD3eBmrkozvX0YDkBnljFYsdn3xU",
-      message: "Thanks for accepting my invite, Liz. Given your experience with managing My Wellness Check documents and coordinating clinical programs at Sylvester Comprehensive Cancer Center, I believe our mission at Simple HealthKit to deliver scalable and affordable testing programs could align well with your work. Would you be free for a 15-minute chat next week to discuss this further?",
-      created_at: "Jan 1st, 2023"
-    },
-    {
-      full_name: "Liz Rivera, MSEd",
-      img_url: "https://media.licdn.com/dms/image/D4E03AQGLYlbY-sTEHw/profile-displayphoto-shrink_400_400/0/1676396253631?e=1703721600&v=beta&t=bfpW2ackeOYLoc5KD3eBmrkozvX0YDkBnljFYsdn3xU",
-      message: "Thanks for accepting my invite, Liz. Given your experience with managing My Wellness Check documents and coordinating clinical programs at Sylvester Comprehensive Cancer Center, I believe our mission at Simple HealthKit to deliver scalable and affordable testing programs could align well with your work. Would you be free for a 15-minute chat next week to discuss this further?",
-      created_at: "Jan 1st, 2023"
-    }
-  ]
+    refetchOnWindowFocus: false,
+  });
+
+
   return (
     <Paper
       p={0}
       pos={'relative'}
       bg={theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white}
     >
-      {/* <LoadingOverlay visible={isFetching} overlayBlur={2} /> */}
+      <LoadingOverlay visible={isFetching} overlayBlur={2} />
       <ScrollArea mih={200}>
         <Text
           className='text-gray-500 text-xs'>
