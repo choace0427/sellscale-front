4916c3840dee566475c44720d2acb92f8d1dd945
choace0427 <jacobkevin0427@gmail.com>
2023-12-21 11:48:54 -0800

    Reply to add scraping data

--- src/components/common/persona/ScrapingReport.tsx
+++ src/components/common/persona/ScrapingReport.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import {
   Container,
   Text,
@@ -14,27 +14,47 @@ import {
   Stack,
   Flex,
   Avatar,
-} from '@mantine/core'
-import { Line } from 'react-chartjs-2'
-import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Tooltip, Legend } from 'chart.js'
-import { IconExternalLink, IconSearch, IconCalendar } from '@tabler/icons'
-import { API_URL } from '@constants/data'
-import { useRecoilValue } from 'recoil'
-import { userTokenState } from '@atoms/userAtoms'
-import DOMPurify from 'isomorphic-dompurify'
-import moment from 'moment'
-import { IconSelector } from '@tabler/icons-react'
+} from "@mantine/core";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import {
+  IconExternalLink,
+  IconSearch,
+  IconCalendar,
+  IconInfoCircle,
+} from "@tabler/icons";
+import { API_URL } from "@constants/data";
+import { useRecoilValue } from "recoil";
+import { userTokenState } from "@atoms/userAtoms";
+import DOMPurify from "isomorphic-dompurify";
+import moment from "moment";
+import { IconInfoSmall, IconSelector } from "@tabler/icons-react";
 
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Tooltip, Legend)
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend
+);
 
 const ScrapingReport = () => {
-  const [analytics, setAnalytics]: any = useState(null)
-  const [currentPage, setCurrentPage]: any = useState(1)
-  const [searchQuery, setSearchQuery] = useState('')
-  const userToken = useRecoilValue(userTokenState)
-  const itemsPerPage = 10
-  const [sortField, setSortField] = useState('upload date')
-  const [sortOrder, setSortOrder] = useState('desc')
+  const [analytics, setAnalytics]: any = useState(null);
+  const [currentPage, setCurrentPage]: any = useState(1);
+  const [searchQuery, setSearchQuery] = useState("");
+  const userToken = useRecoilValue(userTokenState);
+  const itemsPerPage = 10;
+  const [sortField, setSortField] = useState("upload date");
+  const [sortOrder, setSortOrder] = useState("desc");
 
   const chartOptions: any = {
     responsive: true,
@@ -52,146 +72,188 @@ const ScrapingReport = () => {
     },
     plugins: {
       legend: {
-        position: 'top',
-        align: 'end',
+        position: "top",
+        align: "end",
         labels: {
           usePointStyle: true,
         },
       },
     },
-  }
+  };
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch(`${API_URL}/analytics/client_upload_analytics`, {
-        method: 'GET',
-        headers: {
-          Authorization: 'Bearer ' + userToken,
-          'Content-Type': 'application/json',
-        },
-      })
-      const data = await response.json()
-      if (data.message === 'Success') {
-        setAnalytics(data.analytics)
+      const response = await fetch(
+        `${API_URL}/analytics/client_upload_analytics`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: "Bearer " + userToken,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await response.json();
+      if (data.message === "Success") {
+        setAnalytics(data.analytics);
       } else {
-        console.error('Failed to fetch analytics data:', data.message)
+        console.error("Failed to fetch analytics data:", data.message);
         // Handle error case here
       }
-    }
+    };
 
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
   if (!analytics) {
     return (
-      <Box w='100%' h='100%' sx={{ textAlign: 'center' }}>
-        <Loader ml='auto' mt='100px' mb='100px' />
+      <Box w="100%" h="100%" sx={{ textAlign: "center" }}>
+        <Loader ml="auto" mt="100px" mb="100px" />
       </Box>
-    )
+    );
   }
 
   // Sorting logic
   const sortedAnalytics = analytics.uploads.sort((a: any, b: any) => {
     if (a[sortField] < b[sortField]) {
-      return sortOrder === 'asc' ? -1 : 1
+      return sortOrder === "asc" ? -1 : 1;
     }
     if (a[sortField] > b[sortField]) {
-      return sortOrder === 'asc' ? 1 : -1
+      return sortOrder === "asc" ? 1 : -1;
     }
-    return 0
-  })
+    return 0;
+  });
 
   // Function to handle sorting
   const handleSort = (field: any) => {
-    setSortField(field)
-    setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc')
-  }
+    setSortField(field);
+    setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+  };
 
   // Pagination logic
-  const startIndex = (currentPage - 1) * itemsPerPage
-  const endIndex = startIndex + itemsPerPage
-  const currentItems = sortedAnalytics.slice(startIndex, endIndex)
-  const theme = useMantineTheme()
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentItems = sortedAnalytics.slice(startIndex, endIndex);
+  const theme = useMantineTheme();
 
   // Search filter function
   const totalFilteredItems = sortedAnalytics.filter((item: any) => {
     return (
-      item['upload name'].toLowerCase().includes(searchQuery.toLowerCase()) ||
-      item['upload date'].toLowerCase().includes(searchQuery.toLowerCase()) ||
-      item['account'].toLowerCase().includes(searchQuery.toLowerCase())
-    )
-  })
-  const filteredItems = totalFilteredItems.slice(startIndex, endIndex)
+      item["upload name"].toLowerCase().includes(searchQuery.toLowerCase()) ||
+      item["upload date"].toLowerCase().includes(searchQuery.toLowerCase()) ||
+      item["account"].toLowerCase().includes(searchQuery.toLowerCase())
+    );
+  });
+  const filteredItems = totalFilteredItems.slice(startIndex, endIndex);
 
   const lineChartData: any = {
     labels: analytics.contacts_over_time.map((entry: any) => entry.x),
     datasets: [
       {
-        label: 'Cumulative Total of Prospects',
+        label: "Cumulative Total of Prospects",
         data: analytics.contacts_over_time.map((entry: any) => entry.y),
         fill: false,
         borderColor: theme.colors.blue[6],
         tension: 0.3,
       },
       {
-        type: 'bar', // Specify 'bar' type for this dataset
-        label: 'Number of Uploads',
+        type: "bar", // Specify 'bar' type for this dataset
+        label: "Number of Uploads",
         data: analytics.contacts_over_time.map((entry: any) => entry.y), // Your uploads per day data
         backgroundColor: theme.colors.grape[0],
         borderColor: theme.colors.grape[3],
         borderWidth: 1,
       },
     ],
-  }
+  };
 
   function getUploadName(uploadName: any) {
     uploadName = uploadName.replace(
-      new RegExp(searchQuery, 'gi'),
-      (match: any) => `<span style="background-color: ${theme.colors.yellow[1]}">${match}</span>`
-    )
-    return uploadName
+      new RegExp(searchQuery, "gi"),
+      (match: any) =>
+        `<span style="background-color: ${theme.colors.yellow[1]}">${match}</span>`
+    );
+    return uploadName;
   }
 
-  console.log(filteredItems)
+  console.log(filteredItems);
 
   return (
-    <Container size='97%' my={'lg'}>
-      <Text fw={600} size={'xl'} mb={'lg'}>
-        Cumulative Scraping Analytics
-      </Text>
-      <Card style={{ width: '100%', height: '400px' }} shadow='md' radius={'md'}>
+    <Container size="97%" my={"lg"}>
+      <Flex justify={"space-between"} mb={"md"} gap={"md"}>
+        <Card
+          shadow="md"
+          radius={"md"}
+          w={"100%"}
+          p={"xl"}
+          style={{ display: "flex", alignItems: "center" }}
+        >
+          <Text fw={600} size={"xl"}>
+            Contacts Scraping Report
+          </Text>
+        </Card>
+        <Card shadow="md" radius={"md"} w={"100%"} p={"xl"}>
+          <Text fw={600} size={"xl"} color="green">
+            ${Math.round(142 * 1.5)} <IconInfoCircle size={14} color="gray" />
+          </Text>
+          <Text fw={600} size={"md"} color="gray">
+            Worth of Contacts Scraped
+          </Text>
+        </Card>
+      </Flex>
+      <Card
+        style={{ width: "100%", height: "500px" }}
+        shadow="md"
+        radius={"md"}
+        pb={60}
+      >
+        <Text fw={600} size={"xl"} color="gray">
+          CONTACTS SCRAPED{" "}
+          <span
+            style={{ color: "black", fontWeight: "600", marginLeft: "10px" }}
+          >
+            ${Math.round(142 * 1.5)}
+          </span>
+        </Text>
         <Line data={lineChartData} options={chartOptions} />
       </Card>
-      <Flex align={'center'} justify={'space-between'} my={'lg'}>
-        <Text fw={600} size={'xl'}>
+      <Flex align={"center"} justify={"space-between"} my={"lg"}>
+        <Text fw={600} size={"xl"}>
           Scraping Report
         </Text>
         <TextInput
-          placeholder='Search'
+          placeholder="Search"
           value={searchQuery}
           onChange={(event) => setSearchQuery(event.currentTarget.value)}
-          rightSection={<IconSearch color='gray' />}
+          rightSection={<IconSearch color="gray" />}
         />
       </Flex>
-      <Table withBorder style={{ borderRadius: '8px' }}>
-        <thead style={{ background: '#f9f9fd', width: '100%' }}>
-          <tr style={{ height: '60px' }}>
-            <th style={{}} onClick={() => handleSort('upload name')}>
-              <div style={{ display: 'flex', alignItems: 'center', gap: 4, cursor: 'pointer' }}>
+      <Table withBorder style={{ borderRadius: "8px" }}>
+        <thead style={{ background: "#f9f9fd", width: "100%" }}>
+          <tr style={{ height: "60px" }}>
+            <th style={{}} onClick={() => handleSort("upload name")}>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: 4,
+                  cursor: "pointer",
+                }}
+              >
                 Upload Name
                 <IconSelector size={14} />
               </div>
             </th>
             <th>SDR</th>
-            <th onClick={() => handleSort('total prospects')}>Scraped</th>
-            <th style={{ textAlign: 'center' }}>Status</th>
-            <th onClick={() => handleSort('upload date')}>
+            <th onClick={() => handleSort("total prospects")}>Scraped</th>
+            <th style={{ textAlign: "center" }}>Status</th>
+            <th onClick={() => handleSort("upload date")}>
               <div
                 style={{
-                  cursor: 'pointer',
-                  justifyContent: 'end',
-                  display: 'flex',
-                  alignItems: 'center',
+                  cursor: "pointer",
+                  justifyContent: "end",
+                  display: "flex",
+                  alignItems: "center",
                   gap: 4,
                 }}
               >
@@ -201,21 +263,26 @@ const ScrapingReport = () => {
             </th>
           </tr>
         </thead>
-        <tbody style={{ background: 'white' }}>
+        <tbody style={{ background: "white" }}>
           {filteredItems.map((upload: any, index: number) => (
             <tr key={index}>
-              <td style={{ width: '25%' }}>
-                <Flex my={4} align={'center'}>
-                  <Avatar size={'md'} mr='xs' radius={'xl'} />
+              <td style={{ width: "25%" }}>
+                <Flex my={4} align={"center"}>
+                  <Avatar size={"md"} mr="xs" radius={"xl"} />
                   <Stack>
-                    <Text size='sm' className=' line-clamp-1'>
-                      {upload['upload name']}
+                    <Text size="sm" className=" line-clamp-1">
+                      {upload["upload name"]}
                     </Text>
                     <Text
-                      size='xs' 
-                      color='blue'
+                      size="xs"
+                      color="blue"
                       mt={-15}
-                      style={{ display: 'flex', alignItems: 'center', gap: 3, cursor: 'pointer' }}
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        gap: 3,
+                        cursor: "pointer",
+                      }}
                     >
                       Associated Campaign
                       <IconExternalLink size={14} />
@@ -223,33 +290,56 @@ const ScrapingReport = () => {
                   </Stack>
                 </Flex>
               </td>
-              <td style={{ width: '20%' }}>
-                <Flex align={'center'}>
-                  <Avatar src={upload?.account_img} size={'sm'} radius={'xl'} mr='xs' />
+              <td style={{ width: "20%" }}>
+                <Flex align={"center"}>
+                  <Avatar
+                    src={upload?.account_img}
+                    size={"sm"}
+                    radius={"xl"}
+                    mr="xs"
+                  />
                   <Stack>
-                    <Text size='xs' style={{ display: 'flex', alignItems: 'center', gap: 3, cursor: 'pointer' }}>
-                      {upload.account} <IconExternalLink size={14} color='#478cef' />
+                    <Text
+                      size="xs"
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        gap: 3,
+                        cursor: "pointer",
+                      }}
+                    >
+                      {upload.account}{" "}
+                      <IconExternalLink size={14} color="#478cef" />
                     </Text>
-                    <Text size='xs' color='gray' mt={-20}>
-                      {upload.account_title?.substr(0, 20)}{' '}{upload.account_title?.length > 20 ? '...' : ''}
+                    <Text size="xs" color="gray" mt={-20}>
+                      {upload.account_title?.substr(0, 20)}{" "}
+                      {upload.account_title?.length > 20 ? "..." : ""}
                     </Text>
                   </Stack>
                 </Flex>
               </td>
-              <td style={{ width: '20%' }}>
-                <Progress value={100} color='green' />
-                <Text size='xs' mt={4}>
-                  {upload.scraped} <span style={{ color: 'gray' }}> out of {upload.scraped}</span>
+              <td style={{ width: "20%" }}>
+                <Progress value={100} color="green" />
+                <Text size="xs" mt={4}>
+                  {upload.scraped}{" "}
+                  <span style={{ color: "gray" }}>
+                    {" "}
+                    out of {upload.scraped}
+                  </span>
                 </Text>
               </td>
-              <td style={{ width: '20%', textAlign: 'center' }}>
-                <Badge color={upload.status === 'Complete' ? 'green' : 'yellow'} variant='outline' size='md'>
+              <td style={{ width: "20%", textAlign: "center" }}>
+                <Badge
+                  color={upload.status === "Complete" ? "green" : "yellow"}
+                  variant="outline"
+                  size="md"
+                >
                   {upload.status}
                 </Badge>
               </td>
-              <td style={{ width: '20%', textAlign: 'right' }}>
-                <IconCalendar size='0.8rem' style={{ marginRight: '4px' }} />{' '}
-                {moment(upload['upload date']).format('MMM D, YYYY')}
+              <td style={{ width: "20%", textAlign: "right" }}>
+                <IconCalendar size="0.8rem" style={{ marginRight: "4px" }} />{" "}
+                {moment(upload["upload date"]).format("MMM D, YYYY")}
               </td>
             </tr>
           ))}
@@ -260,10 +350,10 @@ const ScrapingReport = () => {
         page={currentPage}
         onChange={setCurrentPage}
         total={Math.ceil(totalFilteredItems.length / itemsPerPage)}
-        style={{ marginTop: '1em' }}
+        style={{ marginTop: "1em" }}
       />
     </Container>
-  )
-}
+  );
+};
 
-export default ScrapingReport
+export default ScrapingReport;
