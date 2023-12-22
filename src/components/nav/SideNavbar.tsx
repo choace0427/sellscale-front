4cf635c4abfb7e926405cd77bbce0f534578779b
choace0427 <jacobkevin0427@gmail.com>
2023-12-22 03:21:40 -0500

    Reply to create Adjust page

--- src/components/nav/SideNavbar.tsx
+++ src/components/nav/SideNavbar.tsx
@@ -1,102 +1,66 @@
-import { ReactNode, useEffect, useState } from "react";
-import {
-  createStyles,
-  Group,
-  getStylesRef,
-  rem,
-  Button,
-  Box,
-  Text,
-  Tooltip,
-  Stack,
-  Divider,
-  Center,
-  Badge,
-} from "@mantine/core";
-import {
-  IconUsers,
-  IconSettings,
-  IconInbox,
-  IconTargetArrow,
-  IconChartHistogram,
-  IconBell,
-  IconMap,
-  IconBooks,
-  IconSearch,
-} from "@tabler/icons-react";
-import { useRecoilValue } from "recoil";
-import { NAV_BAR_SIDE_WIDTH } from "@constants/data";
-import ProfileIcon from "@nav/ProfileIcon";
-import { userDataState, userTokenState } from "@atoms/userAtoms";
-import { isLoggedIn } from "@auth/core";
-import { navigateToPage } from "@utils/documentChange";
-import { useNavigate } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
-import { getOnboardingCompletionReport } from "@utils/requests/getOnboardingCompletionReport";
-import { useHover } from "@mantine/hooks";
-import _ from "lodash";
-import { openContextModal } from "@mantine/modals";
-import { LogoFull } from "./Logo";
-import { SearchBar } from "../../../legacy_code/old/SearchBar";
-import { openSpotlight } from "@mantine/spotlight";
+import { ReactNode, useEffect, useState } from 'react';
+import { createStyles, Group, getStylesRef, rem, Button, Box, Text, Tooltip, Stack, Divider, Center, Badge } from '@mantine/core';
+import { IconUsers, IconSettings, IconInbox, IconTargetArrow, IconChartHistogram, IconBell, IconMap, IconBooks, IconSearch } from '@tabler/icons-react';
+import { useRecoilValue } from 'recoil';
+import { NAV_BAR_SIDE_WIDTH } from '@constants/data';
+import ProfileIcon from '@nav/ProfileIcon';
+import { userDataState, userTokenState } from '@atoms/userAtoms';
+import { isLoggedIn } from '@auth/core';
+import { navigateToPage } from '@utils/documentChange';
+import { useNavigate } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { getOnboardingCompletionReport } from '@utils/requests/getOnboardingCompletionReport';
+import { useHover } from '@mantine/hooks';
+import _ from 'lodash';
+import { openContextModal } from '@mantine/modals';
+import { LogoFull } from './Logo';
+import { SearchBar } from '../../../legacy_code/old/SearchBar';
+import { openSpotlight } from '@mantine/spotlight';
 import { getPreOnboardingData } from '@pages/PreOnboarding';
-import { IconHome, IconTarget } from '@tabler/icons';
+import { IconHome, IconPencil, IconTarget } from '@tabler/icons';
 
 const useStyles = createStyles((theme) => ({
   navbar: {
-    backgroundColor: theme.fn.variant({ variant: "filled", color: "dark" })
-      .background,
+    backgroundColor: theme.fn.variant({ variant: 'filled', color: 'dark' }).background,
   },
 
   header: {
     marginBottom: theme.spacing.md,
-    borderTop: `${rem(1)} solid ${theme.fn.lighten(
-      theme.fn.variant({ variant: "filled", color: "dark" }).background!,
-      0.1
-    )}`,
+    borderTop: `${rem(1)} solid ${theme.fn.lighten(theme.fn.variant({ variant: 'filled', color: 'dark' }).background!, 0.1)}`,
   },
 
   footer: {
     paddingTop: theme.spacing.md,
     marginTop: theme.spacing.md,
-    borderTop: `${rem(1)} solid ${theme.fn.lighten(
-      theme.fn.variant({ variant: "filled", color: "dark" }).background!,
-      0.1
-    )}`,
+    borderTop: `${rem(1)} solid ${theme.fn.lighten(theme.fn.variant({ variant: 'filled', color: 'dark' }).background!, 0.1)}`,
   },
 
   link: {
     ...theme.fn.focusStyles(),
-    display: "flex",
-    alignItems: "center",
-    textDecoration: "none",
+    display: 'flex',
+    alignItems: 'center',
+    textDecoration: 'none',
     fontSize: theme.fontSizes.sm,
     color: theme.white,
     padding: `${theme.spacing.xs} ${theme.spacing.sm}`,
     borderRadius: theme.radius.sm,
     fontWeight: 500,
 
-    "&:hover": {
-      backgroundColor: theme.fn.lighten(
-        theme.fn.variant({ variant: "filled", color: "dark" }).background!,
-        0.1
-      ),
+    '&:hover': {
+      backgroundColor: theme.fn.lighten(theme.fn.variant({ variant: 'filled', color: 'dark' }).background!, 0.1),
     },
   },
 
   linkIcon: {
-    ref: getStylesRef("icon"),
+    ref: getStylesRef('icon'),
     color: theme.white,
     opacity: 0.75,
     marginRight: theme.spacing.sm,
   },
   linkActive: {
-    "&, &:hover": {
-      backgroundColor: theme.fn.lighten(
-        theme.fn.variant({ variant: "filled", color: "dark" }).background!,
-        0.15
-      ),
-      [`& .${getStylesRef("icon")}`]: {
+    '&, &:hover': {
+      backgroundColor: theme.fn.lighten(theme.fn.variant({ variant: 'filled', color: 'dark' }).background!, 0.15),
+      [`& .${getStylesRef('icon')}`]: {
         opacity: 0.9,
       },
       color: theme.white,
@@ -105,23 +69,23 @@ const useStyles = createStyles((theme) => ({
 
   setupLink: {
     ...theme.fn.focusStyles(),
-    display: "flex",
-    alignItems: "center",
-    textDecoration: "none",
+    display: 'flex',
+    alignItems: 'center',
+    textDecoration: 'none',
     fontSize: theme.fontSizes.sm,
     color: theme.white,
     padding: `${theme.spacing.xs} ${theme.spacing.sm}`,
     borderRadius: theme.radius.xl,
     fontWeight: 500,
 
-    "&:hover": {
+    '&:hover': {
       backgroundColor: theme.colors.green[9],
     },
   },
   setupLinkActive: {
-    "&, &:hover": {
-      filter: "brightness(1.2)",
-      [`& .${getStylesRef("icon")}`]: {
+    '&, &:hover': {
+      filter: 'brightness(1.2)',
+      [`& .${getStylesRef('icon')}`]: {
         opacity: 0.9,
       },
       color: theme.white,
@@ -134,28 +98,25 @@ export default function SideNavbar(props: {}) {
   const userToken = useRecoilValue(userTokenState);
   const navigate = useNavigate();
   const [fetchedPreOnboardingData, setFetchedPreOnboardingData] = useState(false);
-  const [preOnboardingData, setPreOnboardingData]= useState({});
+  const [preOnboardingData, setPreOnboardingData] = useState({});
 
   const { data, isFetching, refetch } = useQuery({
     queryKey: [`query-sdr-onboarding-completion-report`],
     queryFn: async () => {
       const response = await getOnboardingCompletionReport(userToken);
-      return response.status === "success" ? response.data : null;
+      return response.status === 'success' ? response.data : null;
     },
     enabled: isLoggedIn(),
   });
 
   useEffect(() => {
     if (!fetchedPreOnboardingData) {
-      getPreOnboardingData(
-        userToken,
-        (data: any) => {
-          setPreOnboardingData(data);
-        }
-      );
+      getPreOnboardingData(userToken, (data: any) => {
+        setPreOnboardingData(data);
+      });
       setFetchedPreOnboardingData(true);
     }
-  }, [])
+  }, []);
 
   // Get percentage from completed steps in report
   let stepsCount = 0;
@@ -170,7 +131,7 @@ export default function SideNavbar(props: {}) {
   // const percentage = Math.round((completedStepsCount / stepsCount) * 100); // old percentage
 
   const NUM_MANDATORY_FIELDS_IN_ONBOARDING = 27;
-  const percentage = Math.min(100, Math.round((Object.keys(preOnboardingData).length / NUM_MANDATORY_FIELDS_IN_ONBOARDING) * 100))
+  const percentage = Math.min(100, Math.round((Object.keys(preOnboardingData).length / NUM_MANDATORY_FIELDS_IN_ONBOARDING) * 100));
 
   return (
     <Group
@@ -185,40 +146,24 @@ export default function SideNavbar(props: {}) {
         <LogoFull />
         <Divider color='dark.4' />
         <Box m='md'>
-          <SideNavbarItem
-            icon={<IconHome size='1.0rem' />}
-            label='Overview'
-            tabKey={['overview']}
-          />
+          <SideNavbarItem icon={<IconHome size='1.0rem' />} label='Overview' tabKey={['overview']} />
           <SideNavbarItem
             icon={<IconInbox size='1.0rem' />}
             label={
               <Group noWrap>
                 <Text>Inbox</Text>
                 {parseInt(localStorage.getItem('inbox-count') ?? '') && (
-                    <Badge sx={{ pointerEvents: 'none' }} variant='filled' size='xs' color='blue'>
-                      {localStorage.getItem('inbox-count')}
-                    </Badge>
-                  )}
+                  <Badge sx={{ pointerEvents: 'none' }} variant='filled' size='xs' color='blue'>
+                    {localStorage.getItem('inbox-count')}
+                  </Badge>
+                )}
               </Group>
             }
             tabKey={['inbox']}
           />
-          <SideNavbarItem
-            icon={<IconTargetArrow size='1.0rem' />}
-            label='Campaigns'
-            tabKey={['campaigns', 'all/campaigns', '']}
-          />
-          <SideNavbarItem
-            icon={<IconUsers size='1.0rem' />}
-            label='Contacts'
-            tabKey={['contacts/overview', 'contacts', 'all/contacts']}
-          />
-          <SideNavbarItem
-            icon={<IconTarget size='1.0rem' />}
-            label='Triggers'
-            tabKey={['triggers', 'create-trigger']}
-          />
+          <SideNavbarItem icon={<IconTargetArrow size='1.0rem' />} label='Campaigns' tabKey={['campaigns', 'all/campaigns', '']} />
+          <SideNavbarItem icon={<IconUsers size='1.0rem' />} label='Contacts' tabKey={['contacts/overview', 'contacts', 'all/contacts']} />
+          <SideNavbarItem icon={<IconTarget size='1.0rem' />} label='Triggers' tabKey={['triggers', 'create-trigger']} />
           <SideNavbarItem
             icon={<IconChartHistogram size='1.0rem' />}
             label='Analytics'
@@ -257,22 +202,15 @@ export default function SideNavbar(props: {}) {
           )}
 
           <Divider color='dark.4' mt='lg' mb='sm' />
-          <SideNavbarItem
-            icon={<IconSearch size='1.0rem' />}
-            label='Search'
-            onClick={openSpotlight}
-          />
+          <SideNavbarItem icon={<IconSearch size='1.0rem' />} label='Search' onClick={openSpotlight} />
           {/* <SideNavbarItem
             icon={<IconBell size="1.0rem" />}
             label="Notifications"
             tabKey={["notifications", "all/recent-activity"]}
           /> */}
           <SideNavbarItem icon={<IconBooks size='1.0rem' />} label='Advanced' tabKey='advanced' />
-          <SideNavbarItem
-            icon={<IconSettings size='1.0rem' />}
-            label='Settings'
-            tabKey='settings'
-          />
+          <SideNavbarItem icon={<IconSettings size='1.0rem' />} label='Settings' tabKey='settings' />
+          <SideNavbarItem icon={<IconPencil />} label='Adjust AI' tabKey='ai-request' />
         </Box>
         <Box>
           <Divider color='dark.4' />
@@ -283,38 +221,26 @@ export default function SideNavbar(props: {}) {
   );
 }
 
-function SideNavbarItem(props: {
-  icon: ReactNode;
-  label: ReactNode;
-  tabKey?: string | string[];
-  onClick?: () => void;
-}) {
+function SideNavbarItem(props: { icon: ReactNode; label: ReactNode; tabKey?: string | string[]; onClick?: () => void }) {
   const navigate = useNavigate();
   const { hovered, ref } = useHover();
 
-  const locParts = location.pathname?.split("/");
-  const activeTab =
-    locParts.length === 2 ? locParts[1] : `${locParts[1]}/${locParts[2]}`;
+  const locParts = location.pathname?.split('/');
+  const activeTab = locParts.length === 2 ? locParts[1] : `${locParts[1]}/${locParts[2]}`;
 
-  const active =
-    (Array.isArray(props.tabKey)
-      ? props.tabKey.includes(activeTab)
-      : activeTab === props.tabKey) || hovered;
+  const active = (Array.isArray(props.tabKey) ? props.tabKey.includes(activeTab) : activeTab === props.tabKey) || hovered;
   return (
     <Box
       ref={ref}
       sx={(theme) => ({
         borderRadius: theme.radius.sm,
-        backgroundColor: active ? theme.colors.blue[3] + "20" : "transparent",
-        cursor: "pointer",
+        backgroundColor: active ? theme.colors.blue[3] + '20' : 'transparent',
+        cursor: 'pointer',
       })}
       onClick={() => {
         props.onClick && props.onClick();
-        if(props.tabKey) {
-          navigateToPage(
-            navigate,
-            "/" + (Array.isArray(props.tabKey) ? props.tabKey[0] : props.tabKey)
-          );
+        if (props.tabKey) {
+          navigateToPage(navigate, '/' + (Array.isArray(props.tabKey) ? props.tabKey[0] : props.tabKey));
         }
       }}
     >
@@ -328,11 +254,7 @@ function SideNavbarItem(props: {
         >
           {props.icon}
         </Center>
-        <Text
-          fz={12}
-          fw={active ? 500 : undefined}
-          c={active ? "gray.0" : "dark.1"}
-        >
+        <Text fz={12} fw={active ? 500 : undefined} c={active ? 'gray.0' : 'dark.1'}>
           {props.label}
         </Text>
       </Group>
@@ -347,20 +269,20 @@ function ProfileCard() {
   return (
     <Group
       spacing={7}
-      m="md"
-      align="flex-start"
+      m='md'
+      align='flex-start'
       noWrap
       ref={ref}
       sx={(theme) => ({
-        cursor: "pointer",
-        backgroundColor: hovered ? theme.colors.dark[4] : "transparent",
+        cursor: 'pointer',
+        backgroundColor: hovered ? theme.colors.dark[4] : 'transparent',
         borderRadius: theme.radius.sm,
         borderTopLeftRadius: 20,
         borderBottomLeftRadius: 20,
       })}
       onClick={() => {
         openContextModal({
-          modal: "account",
+          modal: 'account',
           title: <></>,
           innerProps: {},
         });
@@ -369,11 +291,11 @@ function ProfileCard() {
       <ProfileIcon />
       <Box>
         <Tooltip label={userData.sdr_name} openDelay={750} withArrow>
-          <Text fw={500} fz={12} c="gray.0">
+          <Text fw={500} fz={12} c='gray.0'>
             {_.truncate(userData.sdr_name, { length: 12 })}
           </Text>
         </Tooltip>
-        <Text fw={500} fz={8} c="dimmed">
+        <Text fw={500} fz={8} c='dimmed'>
           Manage Account
         </Text>
       </Box>
