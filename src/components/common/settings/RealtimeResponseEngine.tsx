c26effceeb42fb3a983b6fe60f9f949479f33892
choace0427 <jacobkevin0427@gmail.com>
2024-02-23 16:25:33 -0500

    Reply to Update Message Automation UI

--- src/components/common/settings/RealtimeResponseEngine.tsx
+++ src/components/common/settings/RealtimeResponseEngine.tsx
@@ -1,7 +1,7 @@
 import { userDataState, userTokenState } from '@atoms/userAtoms';
 import { syncLocalStorage } from '@auth/core';
-import { Box, Card, Flex, Switch, Text, Title, UnstyledButton } from '@mantine/core';
-import { IconExternalLink } from '@tabler/icons';
+import { Box, Card, Flex, Select, Switch, Text, Title, UnstyledButton } from '@mantine/core';
+import { IconExternalLink, IconInfoCircle } from '@tabler/icons';
 import { updateClientSDR } from '@utils/requests/updateClientSDR';
 import { booleanFilterFn, DataGrid, stringFilterFn } from 'mantine-data-grid';
 import { useRecoilState, useRecoilValue } from 'recoil';
@@ -25,14 +25,7 @@ export default function RealtimeResponseEngine() {
     | undefined;
 
   const update = async (meta_data: Record<string, any>) => {
-    const response = await updateClientSDR(
-      userToken,
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      meta_data
-    );
+    const response = await updateClientSDR(userToken, undefined, undefined, undefined, undefined, meta_data);
     await syncLocalStorage(userToken, setUserData);
   };
 
@@ -43,16 +36,14 @@ export default function RealtimeResponseEngine() {
       emoji: '👍🏽',
       label: 'Objection',
       key: 'use_objections',
-      content:
-        "When prospects have hesitations. SellScale's AI uses a top objection-handling model to save deals.",
+      content: "When prospects have hesitations. SellScale's AI uses a top objection-handling model to save deals.",
       automated: sdrMetadata?.response_options?.use_objections ?? true,
     },
     {
       emoji: '🔥',
       label: 'Next steps',
       key: 'use_next_steps',
-      content:
-        'When further action is required in the convo. SellScale AI will do its best to engage.',
+      content: 'When further action is required in the convo. SellScale AI will do its best to engage.',
       automated: sdrMetadata?.response_options?.use_next_steps ?? true,
     },
     {
@@ -66,24 +57,21 @@ export default function RealtimeResponseEngine() {
       emoji: '❓',
       label: 'Question',
       key: 'use_questions',
-      content:
-        'When prospects have a question. SellScale will answer generic questions, but specific ones like pricing will be redirected.',
+      content: 'When prospects have a question. SellScale will answer generic questions, but specific ones like pricing will be redirected.',
       automated: sdrMetadata?.response_options?.use_questions ?? true,
     },
     {
       emoji: '🔄',
       label: 'Circle Back',
       key: 'use_circle_back',
-      content:
-        'When a prospect has not responded to a previous message. SellScale will use this status to re-engage.',
+      content: 'When a prospect has not responded to a previous message. SellScale will use this status to re-engage.',
       automated: sdrMetadata?.response_options?.use_circle_back ?? true,
     },
     {
       emoji: '🗓️',
       label: 'Scheduling',
       key: 'use_scheduling',
-      content:
-        'When a prospect moves to scheduling, SellScale will help schedule the conversation.',
+      content: 'When a prospect moves to scheduling, SellScale will help schedule the conversation.',
       automated: sdrMetadata?.response_options?.use_scheduling ?? true,
     },
   ];
@@ -97,6 +85,26 @@ export default function RealtimeResponseEngine() {
             SellScale AI's responses <IconExternalLink color='#228be6' size={'1rem'} />
           </a>
         </Text>
+      </Flex>
+      <Flex gap={'4px'} direction={'column'} mt={'sm'}>
+        <Text fz='sm' fw={700}>
+          Sellscale AI Response Time:
+        </Text>
+        <Flex gap={'xs'} align={'center'} fz='sm' style={{ color: 'gray' }}>
+          Wait{' '}
+          <Select
+            data={[
+              { value: '0', label: '0 hours' },
+              { value: '3', label: '3 hours' },
+              { value: '8', label: '8 hours' },
+              { value: '24', label: '24 hours' },
+            ]}
+            size='xs'
+            style={{ width: '100px' }}
+            defaultValue='0'
+          />{' '}
+          before attempting to respond
+        </Flex>
       </Flex>
       <DataGrid
         data={data}
@@ -115,19 +123,12 @@ export default function RealtimeResponseEngine() {
             accessorKey: 'reply_rate',
             header: `Reply Label`,
             enableSorting: true,
-            maxSize: 180,
+            // maxSize: 180,
             filterFn: stringFilterFn,
             cell: (cell) => {
-              const { emoji, label } = cell.row.original;
+              const { emoji, label, content } = cell.row.original;
               return (
-                <Flex
-                  justify={'center'}
-                  align={'center'}
-                  direction={'row'}
-                  gap={4}
-                  w={'100%'}
-                  h={'100%'}
-                >
+                <Flex justify={'center'} align={'center'} direction={'row'} gap={4} w={'100%'} h={'100%'}>
                   <div
                     style={{
                       backgroundColor: '#ECF3FE',
@@ -141,56 +142,52 @@ export default function RealtimeResponseEngine() {
                   >
                     {emoji}
                   </div>
-                  <Text fw={700} size={'lg'} ml='4px'>
-                    {label}
-                  </Text>
+                  <Flex direction={'column'}>
+                    <Flex align={'center'} gap={'xs'}>
+                      <Text fw={700} size={'sm'} ml='4px'>
+                        {label}
+                      </Text>
+                      <IconInfoCircle color='gray' size={'0.8rem'} style={{ marginTop: '-3px' }} />
+                    </Flex>
+                    <Text ml='4px'>{content}</Text>
+                  </Flex>
                 </Flex>
               );
             },
           },
-          {
-            accessorKey: 'about',
-            header: 'About',
-            enableSorting: true,
-            cell: (cell) => {
-              const { content } = cell.row.original;
+          // {
+          //   accessorKey: 'about',
+          //   header: 'About',
+          //   enableSorting: true,
+          //   cell: (cell) => {
+          //     const { content } = cell.row.original;
 
-              return (
-                <Flex
-                  justify={'center'}
-                  align={'center'}
-                  direction={'column'}
-                  gap={4}
-                  w={'100%'}
-                  h={'100%'}
-                >
-                  <Text
-                    fw={500}
-                    size={'sm'}
-                    color='gray'
-                    lineClamp={3}
-                    className=' whitespace-pre-line'
-                  >
-                    {content}
-                  </Text>
-                </Flex>
-              );
-            },
-            filterFn: stringFilterFn,
-          },
+          //     return (
+          //       <Flex justify={'center'} align={'center'} direction={'column'} gap={4} w={'100%'} h={'100%'}>
+          //         <Text fw={500} size={'sm'} color='gray' lineClamp={3} className=' whitespace-pre-line'>
+          //           {content}
+          //         </Text>
+          //       </Flex>
+          //     );
+          //   },
+          //   filterFn: stringFilterFn,
+          // },
           {
             accessorKey: 'respond_ai',
             header: `Respond with AI`,
             enableSorting: true,
-            maxSize: 200,
+            maxSize: 230,
             cell: (cell) => {
               const { automated } = cell.row.original;
 
               return (
-                <Box>
-                  <Flex justify={'left'} align={'left'} gap={'sm'} w={'100%'} h={'100%'}>
-                    <Text fw={500} color='gray' lineClamp={3}>
-                      SellScale AI:
+                <Box w={'100%'}>
+                  <Flex justify={'space-between'} align={'center'} gap={'sm'} w={'100%'} h={'100%'} pt={'8px'}>
+                    {/* <Text fw={500} color='gray' lineClamp={3}>
+                      Respond With SellScale AI:
+                    </Text> */}
+                    <Text fz='xs' color={automated ? '#228be6' : 'gray'} fw={600} w={'100%'}>
+                      Respond{automated ? ' with SellScale AI' : ' by myself'}
                     </Text>
                     <Switch
                       defaultChecked={automated}
@@ -205,12 +202,6 @@ export default function RealtimeResponseEngine() {
                       }}
                     />
                   </Flex>
-                  <Text fz='xs' color='gray' fw={500} mt='8px'>
-                    Respond{automated ? ' with ' : ' by '}
-                    <b style={{ color: automated ? '#2F98C1' : '#333' }}>
-                      {automated ? 'SellScale AI' : 'myself'}
-                    </b>
-                  </Text>
                 </Box>
               );
             },
