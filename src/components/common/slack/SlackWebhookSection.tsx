0172df7d0ba8e7e172aef2e4e35d61af67aacfa9
David Wei <49255196+davidmwei@users.noreply.github.com>
2024-03-04 18:08:15 -0700

    Slack Notifs + Ghost Preview (#996)
    * Move SlackBot related code into SlackbotSection
    
    Out of SlackNotifications.tsx
    
    * Add SlackNotification new component into SlackWebhookSection
    
    * Update SlackbotSection.tsx
    
    * Ghost Preview

--- src/components/common/slack/SlackWebhookSection.tsx
+++ src/components/common/slack/SlackWebhookSection.tsx
@@ -1,21 +1,34 @@
-import { userDataState, userTokenState } from '@atoms/userAtoms';
-import { syncLocalStorage } from '@auth/core';
-import { Title, Text, Paper, Container, TextInput, Button, Loader, Flex, Box, Image, Divider } from '@mantine/core';
-import { showNotification } from '@mantine/notifications';
-import { getSDR } from '@utils/requests/getClientSDR';
-import { patchSlackWebhook } from '@utils/requests/patchSlackWebhook';
-import { sendTestSlackWebhook } from '@utils/requests/sendTestSlackWebhook';
-import { useEffect, useState } from 'react';
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { userDataState, userTokenState } from "@atoms/userAtoms";
+import { syncLocalStorage } from "@auth/core";
+import {
+  Title,
+  Text,
+  Paper,
+  Container,
+  TextInput,
+  Button,
+  Loader,
+  Flex,
+  Box,
+  Image,
+  Divider,
+} from "@mantine/core";
+import { showNotification } from "@mantine/notifications";
+import { getSDR } from "@utils/requests/getClientSDR";
+import { patchSlackWebhook } from "@utils/requests/patchSlackWebhook";
+import { sendTestSlackWebhook } from "@utils/requests/sendTestSlackWebhook";
+import { useEffect, useState } from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
 
-import SlackLogo from '@assets/images/slack-logo.png'
+import SlackLogo from "@assets/images/slack-logo.png";
+import { CustomizeSlackNotifications } from "./SlackNotifications";
 
 export default function SlackbotSection() {
   const userToken = useRecoilValue(userTokenState);
   const [userData, setUserData] = useRecoilState(userDataState);
   const [loading, setLoading] = useState<boolean>(false);
 
-  const [webhook, setWebhook] = useState<string>('');
+  const [webhook, setWebhook] = useState<string>("");
 
   useEffect(() => {
     syncLocalStorage(userToken, setUserData).then(() => {
@@ -27,18 +40,18 @@ export default function SlackbotSection() {
     setLoading(true);
 
     const result = await patchSlackWebhook(userToken, webhook);
-    if (result.status === 'success') {
+    if (result.status === "success") {
       showNotification({
-        title: 'Success',
-        message: 'Slack Webhook updated',
-        color: 'green',
+        title: "Success",
+        message: "Slack Webhook updated",
+        color: "green",
         autoClose: 5000,
       });
     } else {
       showNotification({
-        title: 'Error',
-        message: 'Slack Webhook not updated',
-        color: 'red',
+        title: "Error",
+        message: "Slack Webhook not updated",
+        color: "red",
         autoClose: 5000,
       });
     }
@@ -51,18 +64,18 @@ export default function SlackbotSection() {
     setLoading(true);
 
     const result = await sendTestSlackWebhook(userToken);
-    if (result.status === 'success') {
+    if (result.status === "success") {
       showNotification({
-        title: 'Success',
-        message: 'Sent notification to Slack',
-        color: 'green',
+        title: "Success",
+        message: "Sent notification to Slack",
+        color: "green",
         autoClose: 5000,
       });
     } else {
       showNotification({
-        title: 'Error',
-        message: 'Failed to send notification to Slack',
-        color: 'red',
+        title: "Error",
+        message: "Failed to send notification to Slack",
+        color: "red",
         autoClose: 5000,
       });
     }
@@ -72,50 +85,70 @@ export default function SlackbotSection() {
 
   return (
     <>
-      <Paper withBorder m='xs' p='lg' radius='md' bg={'#fcfcfd'}>
-        <Flex justify={'space-between'} align='center'>
-          <Flex align={'center'} gap={'sm'}>
-            <Image src={SlackLogo} alt='slack' width={25} height={25} />
-            <Flex direction={'column'}>
+      <Paper withBorder m="xs" p="lg" radius="md" bg={"#fcfcfd"}>
+        <Flex justify={"space-between"} align="center">
+          <Flex align={"center"} gap={"sm"}>
+            <Image src={SlackLogo} alt="slack" width={25} height={25} />
+            <Flex direction={"column"}>
               <Text fw={600}>Use Custom Webhook</Text>
             </Flex>
           </Flex>
           <Button
-            disabled={webhook === userData.client.pipeline_notifications_webhook_url}
+            disabled={
+              webhook === userData.client.pipeline_notifications_webhook_url
+            }
             onClick={() => triggerPatchSlackWebhook()}
             loading={loading}
           >
             Save
           </Button>
         </Flex>
-        <Divider mt='md'/>
+        <Divider mt="md" />
 
-        <Flex direction='column'>
+        <Flex direction="column">
           {loading ? (
-            <Loader variant='dots' mt='md' />
+            <Loader variant="dots" mt="md" />
           ) : (
             <TextInput
-              mt='sm'
+              mt="sm"
               value={webhook}
-              placeholder='https://hooks.slack.com/services/...'
+              placeholder="https://hooks.slack.com/services/..."
               onChange={(event) => setWebhook(event.currentTarget.value)}
-              error={webhook && (webhook?.startsWith('https://hooks.slack.com/services/') ? false : 'Please enter a valid Slack Webhook URL')}
+              error={
+                webhook &&
+                (webhook?.startsWith("https://hooks.slack.com/services/")
+                  ? false
+                  : "Please enter a valid Slack Webhook URL")
+              }
             />
           )}
 
           {userData.client.pipeline_notifications_webhook_url && (
-            <Box my='md'>
-              <Button onClick={triggerSendSlackWebhook}>Send Test Notification</Button>
+            <Box my="md">
+              <Button onClick={triggerSendSlackWebhook}>
+                Send Test Notification
+              </Button>
             </Box>
           )}
         </Flex>
 
-        <Text color='gray' size='xs' fw={500}>
-          Access Slack Webhooks by{' '}
-          <a href='https://api.slack.com/apps/A046N8QCQUA/incoming-webhooks?success=1' target='_blank' className='text-[#468af0]'>
+        <Text color="gray" size="xs" fw={500}>
+          Access Slack Webhooks by{" "}
+          <a
+            href="https://api.slack.com/apps/A046N8QCQUA/incoming-webhooks?success=1"
+            target="_blank"
+            className="text-[#468af0]"
+          >
             visiting this link.
           </a>
         </Text>
+
+        {userData.client.pipeline_notifications_webhook_url && (
+          <>
+            <Divider my="lg" />
+            <CustomizeSlackNotifications />
+          </>
+        )}
       </Paper>
     </>
   );
