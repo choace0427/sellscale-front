f7b595ffdc82efce06418ac99bd6d79f685fb230
Aaron Cassar <aaron@sellscale.com>
2024-03-08 09:19:52 +0100

    Added dash card fixes

--- src/components/pages/Overview/OperatorDash/TaskHandlers/TaskHandlerVoiceBuilder.tsx
+++ src/components/pages/Overview/OperatorDash/TaskHandlers/TaskHandlerVoiceBuilder.tsx
@@ -8,5 +8,9 @@ interface TaskHandlerVoiceBuilderData {
 }
 
 export const TaskHandlerVoiceBuilder = (props: TaskHandlerVoiceBuilderData) => {
+  if (!props.data.campaign_id) {
+    return <></>;
+  }
+
   return <VoiceBuilderSection archetypeId={props.data.campaign_id} regenOffset={65} />;
 };
