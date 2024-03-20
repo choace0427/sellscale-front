35e016f4bf1067353ff37f321e50a311ee7bde89
Aaron Cassar <aaron@sellscale.com>
2024-03-19 19:21:26 -0700

    Fixed bug

--- src/components/pages/Overview/OperatorDash/TaskHandlers/TaskHandlerSchedulingFeedback.tsx
+++ src/components/pages/Overview/OperatorDash/TaskHandlers/TaskHandlerSchedulingFeedback.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useRecoilValue } from 'recoil';
 import { userDataState, userTokenState } from '@atoms/userAtoms';
 import SchedulingReview from '@pages/SchedulingReview';
+import { ProspectDetails } from '@pages/ProspectDetail';
 
 interface TaskHandlerSchedulingFeedbackData {
   data: {
@@ -16,11 +17,12 @@ export const TaskHandlerSchedulingFeedback = (props: TaskHandlerSchedulingFeedba
   const userToken = useRecoilValue(userTokenState);
 
   return (
-    <SchedulingReview
-      prospect_id={props.data.prospect_id}
-      prospect_full_name={props.data.prospect_full_name}
-      prospect_demo_date_formatted={props.data.prospect_demo_date_formatted}
-      onTaskComplete={props.onTaskComplete}
-    />
+    // <SchedulingReview
+    //   prospect_id={props.data.prospect_id}
+    //   prospect_full_name={props.data.prospect_full_name}
+    //   prospect_demo_date_formatted={props.data.prospect_demo_date_formatted}
+    //   onTaskComplete={props.onTaskComplete}
+    // />
+    <ProspectDetails prospectId={props.data.prospect_id} />
   );
 };
