badb1457157ce8582fe10ab4f221a0ea97be8fbc
Aaron Cassar <aaron@sellscale.com>
2024-03-21 21:04:17 -0700

    Frontend tweaks

--- src/components/common/persona/OngoingScrapes.tsx
+++ src/components/common/persona/OngoingScrapes.tsx
@@ -1,13 +1,36 @@
 import { userTokenState } from '@atoms/userAtoms';
-import { Group, Progress, Stack, Title, Text, Box, Container } from '@mantine/core';
+import {
+  Group,
+  Progress,
+  Stack,
+  Title,
+  Text,
+  Box,
+  Container,
+  Switch,
+  Button,
+  Anchor,
+  TextInput,
+  ActionIcon,
+  Center,
+  LoadingOverlay,
+} from '@mantine/core';
+import { IconAdjustments, IconReload } from '@tabler/icons';
 import { useQuery } from '@tanstack/react-query';
-import { getApolloScrapes } from '@utils/requests/apolloScrapes';
+import {
+  getApolloScrapes,
+  updateApolloScrape,
+  upsertAndRunApolloScrape,
+} from '@utils/requests/apolloScrapes';
 import { DataTable } from 'mantine-datatable';
+import { useState } from 'react';
 import { useRecoilValue } from 'recoil';
 import { ApolloScrape } from 'src';
 
 export default function OngoingScrapes() {
   const userToken = useRecoilValue(userTokenState);
+
+  const [loading, setLoading] = useState(false);
 
   const { data: scrapes } = useQuery({
     queryKey: [`query-get-apollo-scrapes`],
@@ -17,10 +40,20 @@ export default function OngoingScrapes() {
     },
   });
 
+  const updateScrape = async (
+    id: number,
+    name?: string,
+    active?: boolean,
+    updateFilters?: boolean
+  ) => {
+    await updateApolloScrape(userToken, id, name, active, updateFilters);
+  };
+
   console.log(scrapes);
 
   return (
     <Container>
+      <LoadingOverlay visible={loading} />
       <Stack>
         <Title order={3}>Ongoing Scrapes</Title>
         <DataTable
@@ -33,6 +66,17 @@ export default function OngoingScrapes() {
             {
               accessor: 'name',
               sortable: true,
+              render: ({ id, name }) => (
+                <>
+                  <TextInput
+                    placeholder='Name'
+                    defaultValue={name}
+                    onChange={async (e) => {
+                      await updateScrape(id, e.currentTarget.value, undefined, undefined);
+                    }}
+                  />
+                </>
+              ),
             },
             {
               accessor: 'page_num',
@@ -42,7 +86,8 @@ export default function OngoingScrapes() {
               render: ({ page_num, max_pages }) => (
                 <Stack spacing={10}>
                   <Text>
-                    Page {page_num} / {max_pages} ({Math.round((page_num / (max_pages ?? 1)) * 100)}
+                    Page {page_num.toLocaleString()} / {max_pages?.toLocaleString()} (
+                    {Math.round((page_num / (max_pages ?? 1)) * 100)}
                     %)
                   </Text>
                   <Progress value={Math.round((page_num / (max_pages ?? 1)) * 100)} />
@@ -58,11 +103,66 @@ export default function OngoingScrapes() {
               accessor: 'archetype_name',
               title: 'Campaign',
               sortable: true,
+              render: ({ archetype_name, archetype_id }) => (
+                <>
+                  {archetype_name && (
+                    <Group spacing={10}>
+                      <Anchor
+                        onClick={() => {
+                          // Set to the campaign page
+                          window.location.href = `/setup/linkedin?campaign_id=${archetype_id}`;
+                        }}
+                      >
+                        {archetype_name}
+                      </Anchor>
+                    </Group>
+                  )}
+                </>
+              ),
             },
             {
               accessor: 'active',
               sortable: true,
-              render: ({ active }) => <Box>{active ? 'True' : 'False'}</Box>,
+              render: ({ id, archetype_id, segment_id, name, active }) => (
+                <Box>
+                  <Switch
+                    size='sm'
+                    defaultChecked={active}
+                    onChange={async (event) => {
+                      if (event.currentTarget.checked) {
+                        await upsertAndRunApolloScrape(userToken, name, archetype_id, segment_id);
+                        await updateScrape(id, undefined, true, undefined);
+                      } else {
+                        await updateScrape(id, undefined, false, undefined);
+                      }
+                    }}
+                    onLabel='ON'
+                    offLabel='OFF'
+                  />
+                </Box>
+              ),
+            },
+            {
+              accessor: 'filters',
+              title: 'Update Filters',
+              render: ({ id }) => (
+                <Center>
+                  <ActionIcon
+                    variant='subtle'
+                    radius='lg'
+                    size='md'
+                    color='blue'
+                    aria-label='Refetch Filters'
+                    onClick={async () => {
+                      setLoading(true);
+                      await updateScrape(id, undefined, undefined, true);
+                      setLoading(false);
+                    }}
+                  >
+                    <IconReload style={{ width: '70%', height: '70%' }} stroke={2} />
+                  </ActionIcon>
+                </Center>
+              ),
             },
           ]}
           records={scrapes ?? []}
